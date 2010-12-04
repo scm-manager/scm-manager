d60133b358d4e7574d89ed383d6d6e0f37896077
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2010-12-04 16:19:36 +0100

    added scm-sample-auth

---
+++ samples/scm-sample-auth/src/main/java/sonia/scm/sample/auth/SampleAuthenticationHandler.java
@@ -0,0 +1,189 @@
+/**
+ * Copyright (c) 2010, Sebastian Sdorra
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of SCM-Manager; nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * http://bitbucket.org/sdorra/scm-manager
+ *
+ */
+
+
+
+package sonia.scm.sample.auth;
+
+//~--- non-JDK imports --------------------------------------------------------
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import sonia.scm.SCMContextProvider;
+import sonia.scm.plugin.ext.Extension;
+import sonia.scm.user.User;
+import sonia.scm.user.UserManager;
+import sonia.scm.util.AssertUtil;
+import sonia.scm.web.security.AuthenticationHandler;
+import sonia.scm.web.security.AuthenticationResult;
+import sonia.scm.web.security.AuthenticationState;
+
+//~--- JDK imports ------------------------------------------------------------
+
+import java.io.IOException;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Sebastian Sdorra
+ */
+@Singleton
+@Extension
+public class SampleAuthenticationHandler implements AuthenticationHandler
+{
+
+  /** Field description */
+  public static final String TYPE = "sample";
+
+  //~--- methods --------------------------------------------------------------
+
+  /**
+   * Method description
+   *
+   *
+   * @param request
+   * @param response
+   * @param username
+   * @param password
+   *
+   * @return
+   */
+  @Override
+  public AuthenticationResult authenticate(HttpServletRequest request,
+          HttpServletResponse response, String username, String password)
+  {
+    AssertUtil.assertIsNotEmpty(username);
+    AssertUtil.assertIsNotEmpty(password);
+
+    return authenticate(username, password);
+  }
+
+  /**
+   * Method description
+   *
+   *
+   * @throws IOException
+   */
+  @Override
+  public void close() throws IOException
+  {
+
+    // nothing todo
+  }
+
+  /**
+   * Method description
+   *
+   *
+   * @param context
+   */
+  @Override
+  public void init(SCMContextProvider context)
+  {
+    addUser(new User("dent", "Arthur Dent", "arthur.dent@hitchhiker.com"));
+    addUser(new User("perfect", "Ford Prefect", "ford.perfect@hitchhiker.com"));
+    addUser(new User("slarti", "Slartibartfaß",
+                     "slartibartfass@hitchhiker.com"));
+    addUser(new User("marvin", "Marvin", "paranoid.android@hitchhiker.com"));
+  }
+
+  //~--- get methods ----------------------------------------------------------
+
+  /**
+   * Method description
+   *
+   *
+   * @return
+   */
+  @Override
+  public String getType()
+  {
+    return TYPE;
+  }
+
+  //~--- methods --------------------------------------------------------------
+
+  /**
+   * Method description
+   *
+   *
+   * @param user
+   */
+  private void addUser(User user)
+  {
+    user.setType(TYPE);
+    userDB.put(user.getName(), user);
+  }
+
+  /**
+   * Method description
+   *
+   *
+   * @param username
+   * @param password
+   *
+   * @return
+   */
+  private AuthenticationResult authenticate(String username, String password)
+  {
+    AuthenticationResult result = null;
+    User dbUser = userDB.get(username);
+
+    if (dbUser != null)
+    {
+      if (password.equals(username.concat("123")))
+      {
+        result = new AuthenticationResult(dbUser);
+      }
+      else
+      {
+        result = AuthenticationResult.FAILED;
+      }
+    }
+    else
+    {
+      result = AuthenticationResult.NOT_FOUND;
+    }
+
+    return result;
+  }
+
+  //~--- fields ---------------------------------------------------------------
+
+  /** Field description */
+  private Map<String, User> userDB = new ConcurrentHashMap<String, User>();
+}
