b06f2ab3a2940d6c396476930b08d2c227f04121
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2010-12-29 18:10:00 +0100

    added an plugin archetype

---
+++ maven/scm-plugin-archetype/src/main/resources/archetype-resources/src/main/resources/scm/sample.hello.js
@@ -0,0 +1,68 @@
+/* *
+ * Copyright (c) 2010, Sebastian Sdorra
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of SCM-Manager; nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * http://bitbucket.org/sdorra/scm-manager
+ * 
+ */
+
+
+function sayHello(){
+  Ext.Ajax.request({
+    url: restUrl + 'hello',
+    method: 'GET',
+    disableCaching: true,
+    success: function(response){
+      var msg = response.responseText;
+      Ext.MessageBox.show({
+        title: 'Hello Message',
+        msg: msg,
+        buttons: Ext.MessageBox.OK,
+        icon: Ext.MessageBox.INFO
+      });
+    },
+    failure: function(){
+      Ext.MessageBox.show({
+        title: 'Error',
+        msg: 'Could not display the hello message',
+        buttons: Ext.MessageBox.OK,
+        icon: Ext.MessageBox.ERROR
+      });
+    }
+  });
+}
+
+loginCallbacks.push(function(){
+  var navPanel = Ext.getCmp('navigationPanel');
+  var count = navPanel.count() - 1;
+  navPanel.insertSection(count, {
+    title: 'Hello World',
+    items: [{
+      label: 'Say Hello',
+      fn: sayHello
+    }]
+  });
+});
