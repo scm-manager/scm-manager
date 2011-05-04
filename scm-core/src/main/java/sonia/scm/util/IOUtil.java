4e73d6524e8fef2024ca5bce04be138e3f1202ad
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2011-05-04 17:05:56 +0200

    added copy method with length to IOUtil

--- scm-core/src/main/java/sonia/scm/util/IOUtil.java
+++ scm-core/src/main/java/sonia/scm/util/IOUtil.java
@@ -192,6 +192,59 @@ public class IOUtil
    * Method description
    *
    *
+   * @param in
+   * @param out
+   * @param bufferSize
+   *
+   * @throws IOException
+   */
+  public static void copy(InputStream in, OutputStream out, int bufferSize, int byteCount)
+          throws IOException
+  {
+    byte buffer[] = new byte[bufferSize];
+    int len = bufferSize;
+
+    if (byteCount >= 0)
+    {
+      while (byteCount > 0)
+      {
+        int max = (byteCount < bufferSize)
+                  ? (int) byteCount
+                  : bufferSize;
+
+        len = in.read(buffer, 0, max);
+
+        if (len == -1)
+        {
+          break;
+        }
+
+        byteCount -= len;
+        out.write(buffer, 0, len);
+      }
+    }
+    else
+    {
+      while (true)
+      {
+        len = in.read(buffer, 0, bufferSize);
+
+        if (len < 0)
+        {
+          break;
+        }
+
+        out.write(buffer, 0, len);
+      }
+    }
+    out.flush();
+  }
+
+  
+  /**
+   * Method description
+   *
+   *
    * @param source
    * @param target
    *
