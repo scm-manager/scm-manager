f632319ad4775172752c3b0211da0a733d9ef371
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2011-05-21 11:42:28 +0200

    added add-members subcommand

---
+++ scm-clients/scm-cli-client/src/main/java/sonia/scm/cli/cmd/AddMembersSubCommand.java
@@ -0,0 +1,149 @@
+/**
+ * Copyright (c) 2010, Sebastian Sdorra
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of SCM-Manager; nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * http://bitbucket.org/sdorra/scm-manager
+ *
+ */
+
+
+
+package sonia.scm.cli.cmd;
+
+//~--- non-JDK imports --------------------------------------------------------
+
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.Option;
+
+import sonia.scm.cli.I18n;
+import sonia.scm.client.GroupClientHandler;
+import sonia.scm.client.ScmClientSession;
+import sonia.scm.group.Group;
+
+//~--- JDK imports ------------------------------------------------------------
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author Sebastian Sdorra
+ */
+@Command("add-members")
+public class AddMembersSubCommand extends TemplateSubCommand
+{
+
+  /**
+   * Method description
+   *
+   *
+   * @return
+   */
+  public List<String> getMembers()
+  {
+    return members;
+  }
+
+  /**
+   * Method description
+   *
+   *
+   * @return
+   */
+  public String getName()
+  {
+    return name;
+  }
+
+  //~--- set methods ----------------------------------------------------------
+
+  /**
+   * Method description
+   *
+   *
+   * @param members
+   */
+  public void setMembers(List<String> members)
+  {
+    this.members = members;
+  }
+
+  /**
+   * Method description
+   *
+   *
+   * @param name
+   */
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+
+  //~--- methods --------------------------------------------------------------
+
+  /**
+   * Method description
+   *
+   */
+  @Override
+  protected void run()
+  {
+    ScmClientSession session = createSession();
+    GroupClientHandler handler = session.getGroupHandler();
+    Group group = handler.get(name);
+
+    if (group != null)
+    {
+      group.getMembers().addAll(members);
+      handler.modify(group);
+
+      Map<String, Object> env = new HashMap<String, Object>();
+
+      env.put("group", group);
+      renderTemplate(env, GetGroupSubCommand.TEMPLATE);
+    }
+    else
+    {
+      output.println(i18n.getMessage(I18n.GROUP_NOT_FOUND));
+    }
+  }
+
+  //~--- fields ---------------------------------------------------------------
+
+  /** Field description */
+  @Option(
+    name = "--member",
+    usage = "optionGroupMember",
+    required = true,
+    aliases = { "-m" }
+  )
+  private List<String> members;
+
+  /** Field description */
+  @Argument(usage = "optionGroupName", required = true)
+  private String name;
+}
