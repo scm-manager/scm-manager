662387575fc13afc5796becc710222489845295e
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2011-08-04 09:46:31 +0200

    use indent for plugin.xml transformation

--- maven/scm-maven-plugin/src/main/java/sonia/scm/maven/FixDescriptorMojo.java
+++ maven/scm-maven-plugin/src/main/java/sonia/scm/maven/FixDescriptorMojo.java
@@ -49,11 +49,8 @@ import org.w3c.dom.NodeList;
 
 import java.io.File;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
@@ -331,6 +328,7 @@ public class FixDescriptorMojo extends AbstractMojo
       Transformer transformer =
         TransformerFactory.newInstance().newTransformer();
 
+      transformer.setOutputProperty(OutputKeys.INDENT, "yes");
       transformer.transform(new DOMSource(document),
                             new StreamResult(descriptor));
     }
