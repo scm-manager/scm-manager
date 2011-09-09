85cb79b3316bc7e7eb225ef8d19917c59ca9a73c
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2011-09-09 11:48:05 +0200

    remove unused imports

--- scm-webapp/src/main/java/sonia/scm/template/FreemarkerTemplateHandler.java
+++ scm-webapp/src/main/java/sonia/scm/template/FreemarkerTemplateHandler.java
@@ -45,12 +45,8 @@ import freemarker.template.TemplateException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import sonia.scm.ConfigurationException;
-import sonia.scm.util.Util;
-
 //~--- JDK imports ------------------------------------------------------------
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.Writer;
