b780e71f35fe543dda0c91ec4a0c90fa50fc7df0
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2011-12-03 16:02:55 +0100

    create api for compare urls

---
+++ scm-plugin-backend/src/main/java/sonia/scm/plugin/rest/url/CompareUrlBuilderFactory.java
@@ -0,0 +1,113 @@
+/**
+ * Copyright (c) 2010, Sebastian Sdorra
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of SCM-Manager; nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * http://bitbucket.org/sdorra/scm-manager
+ *
+ */
+
+
+
+package sonia.scm.plugin.rest.url;
+
+//~--- non-JDK imports --------------------------------------------------------
+
+import com.google.inject.Singleton;
+
+import sonia.scm.plugin.PluginInformation;
+import sonia.scm.util.Util;
+
+//~--- JDK imports ------------------------------------------------------------
+
+import java.util.Set;
+
+/**
+ *
+ * @author Sebastian Sdorra
+ */
+@Singleton
+public class CompareUrlBuilderFactory
+{
+
+  /**
+   * Constructs ...
+   *
+   *
+   * @param urlBuilderSet
+   */
+  public CompareUrlBuilderFactory(Set<CompareUrlBuilder> urlBuilderSet)
+  {
+    this.urlBuilderSet = urlBuilderSet;
+  }
+
+  //~--- methods --------------------------------------------------------------
+
+  /**
+   * Method description
+   *
+   *
+   * @param pluginUrl
+   *
+   * @return
+   */
+  public CompareUrlBuilder createCompareUrlBuilder(String pluginUrl)
+  {
+    CompareUrlBuilder builder = null;
+
+    if (Util.isNotEmpty(pluginUrl) && Util.isNotEmpty(urlBuilderSet))
+    {
+      for (CompareUrlBuilder cup : urlBuilderSet)
+      {
+        if (cup.isCompareable(pluginUrl))
+        {
+          builder = cup;
+
+          break;
+        }
+      }
+    }
+
+    return builder;
+  }
+
+  /**
+   * Method description
+   *
+   *
+   * @param plugin
+   *
+   * @return
+   */
+  public CompareUrlBuilder createCompareUrlBuilder(PluginInformation plugin)
+  {
+    return createCompareUrlBuilder(plugin.getUrl());
+  }
+
+  //~--- fields ---------------------------------------------------------------
+
+  /** Field description */
+  private Set<CompareUrlBuilder> urlBuilderSet;
+}
