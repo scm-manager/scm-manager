8b8ee8d5da6ccba48771102faf8a5bcd8ad601ca
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2012-01-08 12:57:55 +0100

    added unit tests for resturl provider

---
+++ scm-core/src/test/java/sonia/scm/url/RestUrlProviderTestBase.java
@@ -0,0 +1,124 @@
+/**
+ * Copyright (c) 2010, Sebastian Sdorra All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer. 2. Redistributions in
+ * binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution. 3. Neither the name of SCM-Manager;
+ * nor the names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * http://bitbucket.org/sdorra/scm-manager
+ *
+ */
+
+
+
+package sonia.scm.url;
+
+/**
+ *
+ * @author Sebastian Sdorra
+ */
+public abstract class RestUrlProviderTestBase extends UrlProviderTestBase
+{
+
+  /** Field description */
+  public static final String URLSUFFIX_RESTAPI = "/api/rest/";
+
+  //~--- get methods ----------------------------------------------------------
+
+  /**
+   * Method description
+   *
+   *
+   * @return
+   */
+  protected abstract String getExtension();
+
+  /**
+   * Method description
+   *
+   *
+   * @param baseUrl
+   *
+   * @return
+   */
+  @Override
+  protected String getExpectedAuthenticationUrl(String baseUrl)
+  {
+    return createRestUrl(baseUrl, "authentication/login");
+  }
+
+  /**
+   * Method description
+   *
+   *
+   * @param baseUrl
+   *
+   * @return
+   */
+  @Override
+  protected String getExpectedConfigUrl(String baseUrl)
+  {
+    return createRestUrl(baseUrl, "config");
+  }
+
+  /**
+   * Method description
+   *
+   *
+   * @param baseUrl
+   *
+   * @return
+   */
+  @Override
+  protected String getExpectedStateUrl(String baseUrl)
+  {
+    return createRestUrl(baseUrl, "authentication");
+  }
+
+  //~--- methods --------------------------------------------------------------
+
+  /**
+   * Method description
+   *
+   *
+   * @param baseUrl
+   *
+   * @return
+   */
+  private String createBaseRestUrl(String baseUrl)
+  {
+    return baseUrl.concat(URLSUFFIX_RESTAPI);
+  }
+
+  /**
+   * Method description
+   *
+   *
+   * @param baseUrl
+   * @param urlPart
+   *
+   * @return
+   */
+  private String createRestUrl(String baseUrl, String urlPart)
+  {
+    return createBaseRestUrl(baseUrl).concat(urlPart).concat(getExtension());
+  }
+}
---
+++ scm-core/src/test/java/sonia/scm/url/UrlProviderTestBase.java
@@ -0,0 +1,158 @@
+/**
+ * Copyright (c) 2010, Sebastian Sdorra All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer. 2. Redistributions in
+ * binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution. 3. Neither the name of SCM-Manager;
+ * nor the names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * http://bitbucket.org/sdorra/scm-manager
+ *
+ */
+
+
+
+package sonia.scm.url;
+
+//~--- non-JDK imports --------------------------------------------------------
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Sebastian Sdorra
+ */
+public abstract class UrlProviderTestBase
+{
+
+  /** Field description */
+  private static final String BASEURL = "http://scm.scm-manager.org/scm";
+
+  //~--- methods --------------------------------------------------------------
+
+  /**
+   * Method description
+   *
+   *
+   *
+   * @param baseUrl
+   * @return
+   */
+  protected abstract UrlProvider createUrlProvider(String baseUrl);
+
+  //~--- get methods ----------------------------------------------------------
+
+  /**
+   * Method description
+   *
+   *
+   * @param baseUrl
+   *
+   * @return
+   */
+  protected abstract String getExpectedAuthenticationUrl(String baseUrl);
+
+  /**
+   * Method description
+   *
+   *
+   * @param baseUrl
+   *
+   * @return
+   */
+  protected abstract String getExpectedConfigUrl(String baseUrl);
+
+  /**
+   * Method description
+   *
+   *
+   * @param baseUrl
+   *
+   * @return
+   */
+  protected abstract String getExpectedStateUrl(String baseUrl);
+
+  //~--- methods --------------------------------------------------------------
+
+  /**
+   * Method description
+   *
+   */
+  @Test
+  public void testGetAuthenticationUrl()
+  {
+    assertEquals(getExpectedAuthenticationUrl(BASEURL),
+                 createUrlProvider(BASEURL).getAuthenticationUrl());
+  }
+
+  /**
+   * Method description
+   *
+   */
+  @Test
+  public void testGetConfigUrl()
+  {
+    assertEquals(getExpectedConfigUrl(BASEURL),
+                 createUrlProvider(BASEURL).getConfigUrl());
+  }
+
+  /**
+   * Method description
+   *
+   */
+  @Test
+  public void testGetGroupUrlProvider()
+  {
+    assertNotNull(createUrlProvider(BASEURL).getGroupUrlProvider());
+  }
+
+  /**
+   * Method description
+   *
+   */
+  @Test
+  public void testGetStateUrl()
+  {
+    assertEquals(getExpectedStateUrl(BASEURL),
+                 createUrlProvider(BASEURL).getStateUrl());
+  }
+
+  /**
+   * Method description
+   *
+   */
+  @Test
+  public void testGetUserRepositoryUrlProvider()
+  {
+    assertNotNull(createUrlProvider(BASEURL).getRepositoryUrlProvider());
+  }
+
+  /**
+   * Method description
+   *
+   */
+  @Test
+  public void testGetUserUrlProvider()
+  {
+    assertNotNull(createUrlProvider(BASEURL).getUserUrlProvider());
+  }
+}
