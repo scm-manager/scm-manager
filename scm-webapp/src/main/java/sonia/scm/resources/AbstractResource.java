ce9284f03133b16ed124d9359d909870a7f92183
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2012-02-03 20:11:04 +0100

    remove unused import

--- scm-webapp/src/main/java/sonia/scm/resources/AbstractResource.java
+++ scm-webapp/src/main/java/sonia/scm/resources/AbstractResource.java
@@ -37,7 +37,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import sonia.scm.boot.BootstrapUtil;
-import sonia.scm.plugin.ScriptResourceServlet;
 import sonia.scm.util.IOUtil;
 import sonia.scm.util.Util;
 
