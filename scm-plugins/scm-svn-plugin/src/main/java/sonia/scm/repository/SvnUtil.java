d42a3c089e96e2cfeb4aeaa47d1b8089ecc991f7
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2012-06-12 15:12:48 +0200

    start implementation of new repository api for svn

--- scm-plugins/scm-svn-plugin/src/main/java/sonia/scm/repository/SvnUtil.java
+++ scm-plugins/scm-svn-plugin/src/main/java/sonia/scm/repository/SvnUtil.java
@@ -89,6 +89,35 @@ public class SvnUtil
   {
     appendModification(modifications, entry.getType(), entry.getPath());
   }
+  
+    /**
+   * Method description
+   *
+   *
+   * @param revision
+   *
+   * @return
+   *
+   * @throws RepositoryException
+   */
+  public static long getRevisionNumber(String revision) throws RepositoryException
+  {
+    long revisionNumber = -1;
+
+    if (Util.isNotEmpty(revision))
+    {
+      try
+      {
+        revisionNumber = Long.parseLong(revision);
+      }
+      catch (NumberFormatException ex)
+      {
+        throw new RepositoryException("given revision is not a svnrevision");
+      }
+    }
+
+    return revisionNumber;
+  }
 
   /**
    * Method description
---
+++ scm-plugins/scm-svn-plugin/src/main/java/sonia/scm/repository/spi/SvnCatCommand.java
@@ -0,0 +1,119 @@
+/**
+ * Copyright (c) 2010, Sebastian Sdorra All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer. 2. Redistributions in
+ * binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution. 3. Neither the name of SCM-Manager;
+ * nor the names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * http://bitbucket.org/sdorra/scm-manager
+ *
+ */
+
+
+
+package sonia.scm.repository.spi;
+
+//~--- non-JDK imports --------------------------------------------------------
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNProperties;
+import org.tmatesoft.svn.core.io.SVNRepository;
+
+import sonia.scm.repository.Repository;
+import sonia.scm.repository.RepositoryException;
+import sonia.scm.repository.SvnUtil;
+
+//~--- JDK imports ------------------------------------------------------------
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ *
+ * @author Sebastian Sdorra
+ */
+public class SvnCatCommand extends AbstractSvnCommand implements CatCommand
+{
+
+  /**
+   * the logger for SvnCatCommand
+   */
+  private static final Logger logger =
+    LoggerFactory.getLogger(SvnCatCommand.class);
+
+  //~--- constructors ---------------------------------------------------------
+
+  /**
+   * Constructs ...
+   *
+   *
+   * @param repository
+   * @param repositoryDirectory
+   */
+  SvnCatCommand(Repository repository, File repositoryDirectory)
+  {
+    super(repository, repositoryDirectory);
+  }
+
+  //~--- get methods ----------------------------------------------------------
+
+  /**
+   * Method description
+   *
+   *
+   * @param request
+   * @param output
+   *
+   * @throws IOException
+   * @throws RepositoryException
+   */
+  @Override
+  public void getCatResult(CatCommandRequest request, OutputStream output)
+          throws IOException, RepositoryException
+  {
+    if (logger.isDebugEnabled())
+    {
+      logger.debug("try to get content for {}", request);
+    }
+
+    long revisionNumber = SvnUtil.getRevisionNumber(request.getRevision());
+    SVNRepository svnRepository = null;
+
+    try
+    {
+      svnRepository = open();
+      svnRepository.getFile(request.getPath(), revisionNumber,
+                            new SVNProperties(), output);
+    }
+    catch (SVNException ex)
+    {
+      logger.error("could not open repository", ex);
+    }
+    finally
+    {
+      SvnUtil.closeSession(svnRepository);
+    }
+  }
+}
