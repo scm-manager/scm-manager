61c2e942be41f624fdb8ec4c01fe3940d91c981c
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2018-05-28 13:21:47 +0200

    Extend user dto

--- scm-webapp/src/main/java/sonia/scm/api/rest/resources/UserDto.java
+++ scm-webapp/src/main/java/sonia/scm/api/rest/resources/UserDto.java
@@ -4,5 +4,13 @@ import lombok.Data;
 
 @Data
 public class UserDto {
+  private boolean active;
+  private boolean admin;
+  private Long creationDate;
+  private String displayName;
+  private Long lastModified;
+  private String mail;
   private String name;
+  private String password;
+  private String type;
 }
