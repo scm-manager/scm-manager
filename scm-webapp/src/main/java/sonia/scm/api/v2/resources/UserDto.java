dc9295a434f1c60436bfdde818443cf803549313
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2018-06-04 12:43:57 +0200

    Add all value constructor for proper deserialization of empty optional

--- scm-webapp/src/main/java/sonia/scm/api/v2/resources/UserDto.java
+++ scm-webapp/src/main/java/sonia/scm/api/v2/resources/UserDto.java
@@ -2,19 +2,21 @@ package sonia.scm.api.v2.resources;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 import java.util.Map;
 import java.util.Optional;
 
-@Data
-@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@Data @AllArgsConstructor @NoArgsConstructor
 public class UserDto {
   private boolean active;
   private boolean admin;
   private Instant creationDate;
   private String displayName;
+  @JsonInclude(JsonInclude.Include.NON_EMPTY)
   private Optional<Instant> lastModified;
   private String mail;
   private String name;
