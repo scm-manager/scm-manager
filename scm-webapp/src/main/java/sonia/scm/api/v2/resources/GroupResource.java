ab7b07c2f9d3e78c67570ac254d00ea0d29a5584
Philipp Czora <philipp.czora@cloudogu.com>
2018-06-27 16:01:43 +0200

    Implemented DELETE for groups

--- scm-webapp/src/main/java/sonia/scm/api/v2/resources/GroupResource.java
+++ scm-webapp/src/main/java/sonia/scm/api/v2/resources/GroupResource.java
@@ -9,13 +9,7 @@ import sonia.scm.group.GroupManager;
 import sonia.scm.web.VndMediaType;
 
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
@@ -77,7 +71,7 @@ public class GroupResource {
   })
   @TypeHint(TypeHint.NO_CONTENT.class)
   public Response delete(@PathParam("id") String name) {
-    throw new UnsupportedOperationException("Not yet implemented");
+    return adapter.delete(name);
   }
 
   /**
