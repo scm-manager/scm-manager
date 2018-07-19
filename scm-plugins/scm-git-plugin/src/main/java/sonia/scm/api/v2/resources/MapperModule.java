94b0fdd21220b25c1f3a620e7e331cd90995fa9a
Michael Behlendorf <michael.behlendorf@cloudogu.com>
2018-07-19 16:09:38 +0200

    Add Extension annotation to GitConfig MapperModule

--- scm-plugins/scm-git-plugin/src/main/java/sonia/scm/api/v2/resources/MapperModule.java
+++ scm-plugins/scm-git-plugin/src/main/java/sonia/scm/api/v2/resources/MapperModule.java
@@ -3,7 +3,9 @@ package sonia.scm.api.v2.resources;
 import com.google.inject.AbstractModule;
 import com.google.inject.servlet.ServletScopes;
 import org.mapstruct.factory.Mappers;
+import sonia.scm.plugin.Extension;
 
+@Extension
 public class MapperModule extends AbstractModule {
   @Override
   protected void configure() {
