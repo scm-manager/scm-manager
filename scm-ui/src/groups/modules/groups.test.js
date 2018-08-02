efda5122f717296a550c3013f54676aee0fa7e5b
Maren Süwer <maren.suewer@cloudogu.com>
2018-08-02 10:06:13 +0200

    refactor names of tests of group.js

--- scm-ui/src/groups/modules/groups.test.js
+++ scm-ui/src/groups/modules/groups.test.js
@@ -398,7 +398,7 @@ describe("selector tests", () => {
     expect(selectListAsCollection(state)).toBe(collection);
   });
 
-  it("should return false", () => {
+  it("should return false when groupCreatePermission is false", () => {
     expect(isPermittedToCreateGroups({})).toBe(false);
     expect(isPermittedToCreateGroups({ groups: { list: { entry: {} } } })).toBe(
       false
@@ -410,7 +410,7 @@ describe("selector tests", () => {
     ).toBe(false);
   });
 
-  it("should return true", () => {
+  it("should return true when groupCreatePermission is true", () => {
     const state = {
       groups: {
         list: {
