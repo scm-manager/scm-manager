8b16a0251a197d42d41e26e8a32875952ceb8e4d
Johannes Schnatterer <johannes.schnatterer@cloudogu.com>
2018-08-02 13:02:43 +0200

    VndMedia Type classes: Adds private constructor

--- scm-plugins/scm-git-plugin/src/main/java/sonia/scm/web/GitVndMediaType.java
+++ scm-plugins/scm-git-plugin/src/main/java/sonia/scm/web/GitVndMediaType.java
@@ -2,4 +2,7 @@ package sonia.scm.web;
 
 public class GitVndMediaType {
   public static final String GIT_CONFIG = VndMediaType.PREFIX + "gitConfig" + VndMediaType.SUFFIX;
+
+  private GitVndMediaType() {
+  }
 }
--- scm-plugins/scm-hg-plugin/src/main/java/sonia/scm/web/HgVndMediaType.java
+++ scm-plugins/scm-hg-plugin/src/main/java/sonia/scm/web/HgVndMediaType.java
@@ -1,7 +1,9 @@
 package sonia.scm.web;
 
-import sonia.scm.web.VndMediaType;
 
 public class HgVndMediaType {
   public static final String HG_CONFIG = VndMediaType.PREFIX + "hgConfig" + VndMediaType.SUFFIX;
+
+  private HgVndMediaType() {
+  }
 }
