98b8e343085a1ad87053c1ee382f21e37e8a7c67
Maren Süwer <maren.suewer@cloudogu.com>
2018-08-21 08:07:09 +0200

    remove unnecessary url

--- scm-ui/src/components/validation.js
+++ scm-ui/src/components/validation.js
@@ -14,17 +14,3 @@ export const isMailValid = (mail: string) => {
 export const isNumberValid = (number: string) => {
   return !isNaN(number);
 };
-
-const urlRegex = new RegExp(
-  "^(https?:\\/\\/)?" + // protocol
-  "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|" + // domain name
-  "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
-  "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
-  "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
-    "(\\#[-a-z\\d_]*)?$",
-  "i"
-); // fragment locator
-
-export const isUrlValid = (url: string) => {
-  return urlRegex.test(url);
-};
