d7a229a08209ae381a8c0b12d82f5f369f52b91c
Philipp Czora <philipp.czora@cloudogu.com>
2018-08-27 14:49:52 +0200

    Removed unused Exception

--- scm-core/src/main/java/sonia/scm/repository/BranchNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package sonia.scm.repository;
-
-import sonia.scm.NotFoundException;
-
-public class BranchNotFoundException extends NotFoundException {
-  public BranchNotFoundException(String branch) {
-    super("branch", branch);
-  }
-}
