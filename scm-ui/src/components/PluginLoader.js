bc2b54a2c071404d4eeab7a290422029c228d20e
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2018-08-30 11:08:36 +0200

    ignore sonar for eval of plugin bundles

--- scm-ui/src/components/PluginLoader.js
+++ scm-ui/src/components/PluginLoader.js
@@ -74,6 +74,7 @@ class PluginLoader extends React.Component<Props, State> {
       })
       .then(script => {
         // TODO is this safe???
+        // NOSONAR
         eval(script);
       });
   };
