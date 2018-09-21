4c468c57432dd48b00dc182fbccd38e17db93400
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2018-09-21 12:16:27 +0200

    Remove unused import

--- scm-webapp/src/main/java/sonia/scm/api/v2/resources/BranchRootResource.java
+++ scm-webapp/src/main/java/sonia/scm/api/v2/resources/BranchRootResource.java
@@ -26,7 +26,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
-import java.text.MessageFormat;
 
 public class BranchRootResource {
 
