987cd54dc709c37d0a2ca03252ff7d4d52296a1c
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2018-09-26 17:33:07 +0200

    Fix missing media type

--- scm-webapp/src/main/java/sonia/scm/api/v2/resources/IndexResource.java
+++ scm-webapp/src/main/java/sonia/scm/api/v2/resources/IndexResource.java
@@ -1,8 +1,12 @@
 package sonia.scm.api.v2.resources;
 
+import com.webcohesion.enunciate.metadata.rs.TypeHint;
+import sonia.scm.web.VndMediaType;
+
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 @Path(IndexResource.INDEX_PATH_V2)
 public class IndexResource {
@@ -16,6 +20,8 @@ public class IndexResource {
   }
 
   @GET
+  @Produces(VndMediaType.INDEX)
+  @TypeHint(IndexDto.class)
   public IndexDto getIndex() {
     return indexDtoGenerator.generate();
   }
