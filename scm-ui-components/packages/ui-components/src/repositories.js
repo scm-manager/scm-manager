11fca51f5fb6af21b9df79ef5a572bf44d5de2e9
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2018-09-27 12:17:36 +0200

    fix missing checkout/clone description on repository information page

---
+++ scm-ui-components/packages/ui-components/src/repositories.js
@@ -0,0 +1,19 @@
+// @flow
+import type { Repository } from "@scm-manager/ui-types";
+
+// util methods for repositories
+
+export function getProtocolLinkByType(repository: Repository, type: string) {
+  let protocols = repository._links.protocol;
+  if (protocols) {
+    if (!Array.isArray(protocols)) {
+      protocols = [protocols];
+    }
+    for (let proto of protocols) {
+      if (proto.name === type) {
+        return proto.href;
+      }
+    }
+  }
+  return null;
+}
