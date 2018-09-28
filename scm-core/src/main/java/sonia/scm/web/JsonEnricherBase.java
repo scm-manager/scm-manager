8f139c8d4ab264ea259077ded30de8cd40fd572b
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2018-09-28 16:53:09 +0200

    Reduce code duplication for json object modification

---
+++ scm-core/src/main/java/sonia/scm/web/JsonEnricherBase.java
@@ -0,0 +1,40 @@
+package sonia.scm.web;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.util.Map;
+
+public abstract class JsonEnricherBase implements JsonEnricher {
+
+  private final ObjectMapper objectMapper;
+
+  protected JsonEnricherBase(ObjectMapper objectMapper) {
+    this.objectMapper = objectMapper;
+  }
+
+  protected boolean resultHasMediaType(String mediaType, JsonEnricherContext context) {
+    return mediaType.equals(context.getResponseMediaType().toString());
+  }
+
+  protected JsonNode value(Object gitConfigUrl) {
+    return objectMapper.convertValue(gitConfigUrl, JsonNode.class);
+  }
+
+  protected ObjectNode createObject() {
+    return objectMapper.createObjectNode();
+  }
+
+  protected ObjectNode createObject(Map<String, Object> values) {
+    ObjectNode object = createObject();
+
+    values.forEach((key, value) -> object.set(key, value(value)));
+
+    return object;
+  }
+
+  protected void addPropertyNode(JsonNode parent, String newKey, JsonNode child) {
+    ((ObjectNode) parent).put(newKey, child);
+  }
+}
--- scm-plugins/scm-git-plugin/src/main/java/sonia/scm/api/v2/resources/GitConfigInIndexResource.java
+++ scm-plugins/scm-git-plugin/src/main/java/sonia/scm/api/v2/resources/GitConfigInIndexResource.java
@@ -2,44 +2,39 @@ package sonia.scm.api.v2.resources;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import sonia.scm.config.ConfigurationPermissions;
 import sonia.scm.plugin.Extension;
-import sonia.scm.web.JsonEnricher;
+import sonia.scm.web.JsonEnricherBase;
 import sonia.scm.web.JsonEnricherContext;
-import sonia.scm.web.VndMediaType;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
 
+import static java.util.Collections.singletonMap;
+import static sonia.scm.web.VndMediaType.INDEX;
+
 @Extension
-public class GitConfigInIndexResource implements JsonEnricher {
+public class GitConfigInIndexResource extends JsonEnricherBase {
 
   private final Provider<ScmPathInfoStore> scmPathInfoStore;
-  private final ObjectMapper objectMapper;
 
   @Inject
   public GitConfigInIndexResource(Provider<ScmPathInfoStore> scmPathInfoStore, ObjectMapper objectMapper) {
+    super(objectMapper);
     this.scmPathInfoStore = scmPathInfoStore;
-    this.objectMapper = objectMapper;
   }
 
   @Override
   public void enrich(JsonEnricherContext context) {
-    if (isIndexRequest(context) && ConfigurationPermissions.list().isPermitted()) {
+    if (resultHasMediaType(INDEX, context) && ConfigurationPermissions.list().isPermitted()) {
       String gitConfigUrl = new LinkBuilder(scmPathInfoStore.get().get(), GitConfigResource.class)
         .method("get")
         .parameters()
         .href();
 
-      ObjectNode gitConfigRefNode = objectMapper.createObjectNode();
-      gitConfigRefNode.set("href", objectMapper.convertValue(gitConfigUrl, JsonNode.class));
+      JsonNode gitConfigRefNode = createObject(singletonMap("href", value(gitConfigUrl)));
 
-      ((ObjectNode) context.getResponseEntity().get("_links")).put("gitConfig", gitConfigRefNode);
+      addPropertyNode(context.getResponseEntity().get("_links"), "gitConfig", gitConfigRefNode);
     }
-  }
-
-  private boolean isIndexRequest(JsonEnricherContext context) {
-    return VndMediaType.INDEX.equals(context.getResponseMediaType().toString());
   }
 }
