f0c69d4f30ac418baa9407f3015e7ce32c8f8841
Mohamed Karray <mohamed.karray.sr@gmail.com>
2018-10-16 13:18:59 +0200

    disable sonar validation with id squid:S2068

--- scm-core/src/main/java/sonia/scm/user/ChangePasswordNotAllowedException.java
+++ scm-core/src/main/java/sonia/scm/user/ChangePasswordNotAllowedException.java
@@ -3,6 +3,7 @@ package sonia.scm.user;
 public class ChangePasswordNotAllowedException extends RuntimeException {
 
   public static final String WRONG_USER_TYPE = "User of type {0} are not allowed to change password";
+  @SuppressWarnings("squid:S2068")
   public static final String OLD_PASSWORD_REQUIRED = "the old password is required.";
 
   public ChangePasswordNotAllowedException(String message) {
