f5f96042b33a5456f7be0fc347ef982b1addeea5
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2018-11-06 16:38:49 +0100

    use global configuration abstraction to implement global configuration for git

--- scm-plugins/scm-git-plugin/src/main/js/index.js
+++ scm-plugins/scm-git-plugin/src/main/js/index.js
@@ -2,8 +2,9 @@
 import { binder } from "@scm-manager/ui-extensions";
 import ProtocolInformation from "./ProtocolInformation";
 import GitAvatar from "./GitAvatar";
-import GitConfigurationNavLink from "./GitConfigurationNavLink";
-import GitConfigurationRoute from "./GitConfigurationRoute";
+
+import { ConfigurationBinder as cfgBinder } from "@scm-manager/ui-components";
+import GitGlobalConfiguration from "./GitGlobalConfiguration";
 
 // repository
 
@@ -16,9 +17,4 @@ binder.bind("repos.repository-avatar", GitAvatar, gitPredicate);
 
 // global config
 
-const gitConfigPredicate = (props: Object) => {
-  return props.links && props.links["gitConfig"];
-};
-
-binder.bind("config.navigation", GitConfigurationNavLink, gitConfigPredicate);
-binder.bind("config.route", GitConfigurationRoute, gitConfigPredicate);
+cfgBinder.bindGlobal("/git", "scm-git-plugin.config.link", "gitConfig", GitGlobalConfiguration);
