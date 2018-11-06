6e32d354cf5d8230023f1a6a7ed1f597d747d1f1
Philipp Czora <philipp.czora@cloudogu.com>
2018-11-06 15:26:25 +0100

    Renamed incorrectly named variable

--- scm-ui/src/users/components/updatePassword.js
+++ scm-ui/src/users/components/updatePassword.js
@@ -1,10 +1,11 @@
 //@flow
 import { apiClient } from "@scm-manager/ui-components";
-const CONTENT_TYPE_USER = "application/vnd.scmm-passwordOverwrite+json;v=2";
+const CONTENT_TYPE_PASSWORD_OVERWRITE =
+  "application/vnd.scmm-passwordOverwrite+json;v=2";
 
 export function updatePassword(url: string, password: string) {
   return apiClient
-    .put(url, { newPassword: password }, CONTENT_TYPE_USER)
+    .put(url, { newPassword: password }, CONTENT_TYPE_PASSWORD_OVERWRITE)
     .then(response => {
       return response;
     })
