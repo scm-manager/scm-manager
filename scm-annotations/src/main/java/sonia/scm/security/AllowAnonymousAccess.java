c173db35a26c901782dc930ea9e88b3b99806987
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2018-11-13 11:14:41 +0100

    Add documentation

--- scm-annotations/src/main/java/sonia/scm/security/AllowAnonymousAccess.java
+++ scm-annotations/src/main/java/sonia/scm/security/AllowAnonymousAccess.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Use this annotation to mark REST resource methods that may be accessed <b>without</b> authentication.
+ * To mark all methods of a complete class you can annotate the class instead.
+ */
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface AllowAnonymousAccess {
