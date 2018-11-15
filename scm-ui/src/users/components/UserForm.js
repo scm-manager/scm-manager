ae16482e619cc9e446fba41bfe3cbd1be67ab95a
Maren Süwer <maren.suewer@cloudogu.com>
2018-11-15 10:05:11 +0100

    only allow to add user if mail is set - backend throws error if mail is not set

--- scm-ui/src/users/components/UserForm.js
+++ scm-ui/src/users/components/UserForm.js
@@ -73,7 +73,8 @@ class UserForm extends React.Component<Props, State> {
       this.state.passwordConfirmationError ||
       this.state.displayNameValidationError ||
       this.isFalsy(user.name) ||
-      this.isFalsy(user.displayName)
+      this.isFalsy(user.displayName) ||
+      this.isFalsy(user.mail)
     );
   };
 
