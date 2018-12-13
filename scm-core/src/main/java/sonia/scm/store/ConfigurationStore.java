c3e6e28262ee1725a719a99093638d862b1a10a3
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2018-12-13 08:18:04 +0100

    Fix type

--- scm-core/src/main/java/sonia/scm/store/ConfigurationStore.java
+++ scm-core/src/main/java/sonia/scm/store/ConfigurationStore.java
@@ -58,7 +58,7 @@ public interface ConfigurationStore<T>
    * Stores the given configuration object to the store.
    *
    *
-   * @param obejct configuration object to store
+   * @param object configuration object to store
    */
-  public void set(T obejct);
+  public void set(T object);
 }
