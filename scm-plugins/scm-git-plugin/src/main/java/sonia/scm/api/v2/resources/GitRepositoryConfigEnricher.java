360cba6c500029ad9ce1c8439639c5db80e06f3c
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2018-12-13 11:32:44 +0100

    Use constant

--- scm-plugins/scm-git-plugin/src/main/java/sonia/scm/api/v2/resources/GitRepositoryConfigEnricher.java
+++ scm-plugins/scm-git-plugin/src/main/java/sonia/scm/api/v2/resources/GitRepositoryConfigEnricher.java
@@ -3,6 +3,7 @@ package sonia.scm.api.v2.resources;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import sonia.scm.plugin.Extension;
+import sonia.scm.repository.GitRepositoryHandler;
 import sonia.scm.repository.NamespaceAndName;
 import sonia.scm.repository.RepositoryManager;
 import sonia.scm.web.JsonEnricherBase;
@@ -43,7 +44,7 @@ public class GitRepositoryConfigEnricher extends JsonEnricherBase {
     String namespace = repositoryNode.get("namespace").asText();
     String name = repositoryNode.get("name").asText();
 
-    if ("git".equals(manager.get(new NamespaceAndName(namespace, name)).getType())) {
+    if (GitRepositoryHandler.TYPE_NAME.equals(manager.get(new NamespaceAndName(namespace, name)).getType())) {
       String repositoryConfigLink = new LinkBuilder(scmPathInfoStore.get().get(), GitConfigResource.class)
         .method("getRepositoryConfig")
         .parameters(namespace, name)
