05c4e722b6524816752ee26a61fcd9f34a419f5a
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2018-12-21 14:05:52 +0100

    Map com.fasterxml.jackson.core.JsonParseException to proper response

--- scm-webapp/src/main/java/sonia/scm/api/JsonParseExceptionMapper.java
+++ scm-webapp/src/main/java/sonia/scm/api/JsonParseExceptionMapper.java
@@ -1,5 +1,6 @@
 package sonia.scm.api;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
@@ -12,21 +13,21 @@ import javax.ws.rs.ext.Provider;
 import java.util.Collections;
 
 @Provider
-public class FallbackExceptionMapper implements ExceptionMapper<Exception> {
+public class JsonParseExceptionMapper implements ExceptionMapper<JsonParseException> {
 
-  private static final Logger logger = LoggerFactory.getLogger(FallbackExceptionMapper.class);
+  private static final Logger logger = LoggerFactory.getLogger(JsonParseExceptionMapper.class);
 
-  private static final String ERROR_CODE = "CmR8GCJb31";
+  private static final String ERROR_CODE = "2VRCrvpL71";
 
   @Override
-  public Response toResponse(Exception exception) {
-    logger.warn("mapping unexpected {} to status code 500", exception.getClass().getName(), exception);
+  public Response toResponse(JsonParseException exception) {
+    logger.trace("got illegal json: {}", exception.getMessage());
     ErrorDto errorDto = new ErrorDto();
-    errorDto.setMessage("internal server error");
+    errorDto.setMessage("illegal json content: " + exception.getMessage());
     errorDto.setContext(Collections.emptyList());
     errorDto.setErrorCode(ERROR_CODE);
     errorDto.setTransactionId(MDC.get("transaction_id"));
-    return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+    return Response.status(Response.Status.BAD_REQUEST)
       .entity(errorDto)
       .type(VndMediaType.ERROR_TYPE)
       .build();
