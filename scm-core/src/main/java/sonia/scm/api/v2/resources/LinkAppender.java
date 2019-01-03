5f80f5c4afe5043e732049e04bcfead86e0a67bc
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2019-01-03 11:18:22 +0100

    extends LinkEnricher api to append link arrays to the response

--- scm-core/src/main/java/sonia/scm/api/v2/resources/LinkAppender.java
+++ scm-core/src/main/java/sonia/scm/api/v2/resources/LinkAppender.java
@@ -15,4 +15,33 @@ public interface LinkAppender {
    * @param href link uri
    */
   void appendOne(String rel, String href);
+
+  /**
+   * Returns a builder which is able to append an array of links to the resource.
+   *
+   * @param rel name of link relation
+   * @return multi link builder
+   */
+  LinkArrayBuilder arrayBuilder(String rel);
+
+
+  /**
+   * Builder for link arrays.
+   */
+  interface LinkArrayBuilder {
+
+    /**
+     * Append an link to the array.
+     *
+     * @param name name of link
+     * @param href link target
+     * @return {@code this}
+     */
+    LinkArrayBuilder append(String name, String href);
+
+    /**
+     * Builds the array and appends the it to the json response.
+     */
+    void build();
+  }
 }
