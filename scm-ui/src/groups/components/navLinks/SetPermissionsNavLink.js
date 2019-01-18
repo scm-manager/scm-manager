0711475d78e9529d2e0286b5a496671b1361ce4a
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2019-01-18 13:21:54 +0100

    Copy user permission administration for groups

---
+++ scm-ui/src/groups/components/navLinks/SetPermissionsNavLink.js
@@ -0,0 +1,28 @@
+//@flow
+import React from "react";
+import { translate } from "react-i18next";
+import type { Group } from "@scm-manager/ui-types";
+import { NavLink } from "@scm-manager/ui-components";
+
+type Props = {
+  t: string => string,
+  group: Group,
+  permissionsUrl: String
+};
+
+class ChangePermissionNavLink extends React.Component<Props> {
+  render() {
+    const { t, permissionsUrl } = this.props;
+
+    if (!this.hasPermissionToSetPermission()) {
+      return null;
+    }
+    return <NavLink label={t("set-permissions-button.label")} to={permissionsUrl} />;
+  }
+
+  hasPermissionToSetPermission = () => {
+    return this.props.group._links.permissions;
+  };
+}
+
+export default translate("groups")(ChangePermissionNavLink);
--- scm-ui/src/groups/containers/SingleGroup.js
+++ scm-ui/src/groups/containers/SingleGroup.js
@@ -11,7 +11,11 @@ import {
 } from "@scm-manager/ui-components";
 import { Route } from "react-router";
 import { Details } from "./../components/table";
-import { DeleteGroupNavLink, EditGroupNavLink } from "./../components/navLinks";
+import {
+  DeleteGroupNavLink,
+  EditGroupNavLink,
+  SetPermissionsNavLink
+} from "./../components/navLinks";
 import type { Group } from "@scm-manager/ui-types";
 import type { History } from "history";
 import {
@@ -27,6 +31,7 @@ import {
 import { translate } from "react-i18next";
 import EditGroup from "./EditGroup";
 import { getGroupsLink } from "../../modules/indexResource";
+import SetGroupPermissions from "../components/SetGroupPermissions";
 
 type Props = {
   name: string,
@@ -102,6 +107,11 @@ class SingleGroup extends React.Component<Props> {
               exact
               component={() => <EditGroup group={group} />}
             />
+            <Route
+              path={`${url}/permissions`}
+              exact
+              component={() => <SetGroupPermissions group={group} />}
+            />
           </div>
           <div className="column">
             <Navigation>
@@ -109,6 +119,10 @@ class SingleGroup extends React.Component<Props> {
                 <NavLink
                   to={`${url}`}
                   label={t("single-group.information-label")}
+                />
+                <SetPermissionsNavLink
+                  group={group}
+                  permissionsUrl={`${url}/permissions`}
                 />
               </Section>
               <Section label={t("single-group.actions-label")}>
