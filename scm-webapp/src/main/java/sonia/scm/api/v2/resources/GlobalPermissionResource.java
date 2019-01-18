5e7405b52d812c54321ceb6cccca937cb2e40853
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2019-01-18 11:28:20 +0100

    Cleanup class

--- scm-webapp/src/main/java/sonia/scm/api/v2/resources/GlobalPermissionResource.java
+++ scm-webapp/src/main/java/sonia/scm/api/v2/resources/GlobalPermissionResource.java
@@ -1,18 +1,13 @@
 package sonia.scm.api.v2.resources;
 
-import lombok.extern.slf4j.Slf4j;
-import sonia.scm.security.AssignedPermission;
 import sonia.scm.security.PermissionDescriptor;
 import sonia.scm.security.SecuritySystem;
 import sonia.scm.web.VndMediaType;
 
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("v2/permissions")
@@ -33,5 +28,3 @@ public class GlobalPermissionResource {
     return Response.ok(new PermissionListDto(permissions)).build();
   }
 }
-
-
