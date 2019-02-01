e8666fa3e861d9c9b1935603e7c9ff0158a6ac0a
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2019-02-01 15:53:13 +0100

    Add disabled flag to download button

--- scm-ui-components/packages/ui-components/src/buttons/DownloadButton.js
+++ scm-ui-components/packages/ui-components/src/buttons/DownloadButton.js
@@ -3,14 +3,15 @@ import React from "react";
 
 type Props = {
   displayName: string,
-  url: string
+  url: string,
+  disabled: boolean
 };
 
 class DownloadButton extends React.Component<Props> {
   render() {
-    const { displayName, url } = this.props;
+    const { displayName, url, disabled } = this.props;
     return (
-      <a className="button is-large is-link" href={url}>
+      <a className="button is-large is-link" href={url} disabled={disabled}>
         <span className="icon is-medium">
           <i className="fas fa-arrow-circle-down" />
         </span>
