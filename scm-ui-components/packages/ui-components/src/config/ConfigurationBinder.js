1c459e994473cc2381dfccab7899da6aa01264eb
Florian Scholdei <florian.scholdei@cloudogu.com>
2019-02-01 16:08:33 +0100

    renamed editusernavlink back + implemented new extension point for repo

--- scm-ui-components/packages/ui-components/src/config/ConfigurationBinder.js
+++ scm-ui-components/packages/ui-components/src/config/ConfigurationBinder.js
@@ -59,13 +59,40 @@ class ConfigurationBinder {
     });
 
     // bind navigation link to extension point
-    binder.bind("repository.subnavigation", RepoNavLink, repoPredicate);
+    binder.bind("repository.navigation", RepoNavLink, repoPredicate);
 
 
     // route for global configuration, passes the current repository to component
     const RepoRoute = ({url, repository, ...additionalProps}) => {
       const link = repository._links[linkName].href;
       return this.route(url + to, <RepositoryComponent repository={repository} link={link} {...additionalProps}/>);
+    };
+
+    // bind config route to extension point
+    binder.bind("repository.route", RepoRoute, repoPredicate);
+  }
+
+  bindRepositorySub(to: string, labelI18nKey: string, linkName: string, RepositoryComponent: any) {
+
+    // create predicate based on the link name of the current repository route
+    // if the linkname is not available, the navigation link and the route are not bound to the extension points
+    const repoPredicate = (props: Object) => {
+      return props.repository && props.repository._links && props.repository._links[linkName];
+    };
+
+    // create NavigationLink with translated label
+    const RepoNavLink = translate(this.i18nNamespace)(({t, url}) => {
+      return this.navLink(url + "/settings" + to, labelI18nKey, t);
+    });
+
+    // bind navigation link to extension point
+    binder.bind("repository.subnavigation", RepoNavLink, repoPredicate);
+
+
+    // route for global configuration, passes the current repository to component
+    const RepoRoute = ({url, repository, ...additionalProps}) => {
+      const link = repository._links[linkName].href;
+      return this.route(url + "/settings" + to, <RepositoryComponent repository={repository} link={link} {...additionalProps}/>);
     };
 
     // bind config route to extension point
