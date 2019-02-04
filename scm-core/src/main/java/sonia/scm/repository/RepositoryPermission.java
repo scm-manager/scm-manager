7369f1cfced2c258ad21778cb3c234a1e91032be
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2019-02-04 16:33:03 +0100

    Ensure that verbs occur only once in the collection
    This is necessary for equals to work correctly

--- scm-core/src/main/java/sonia/scm/repository/RepositoryPermission.java
+++ scm-core/src/main/java/sonia/scm/repository/RepositoryPermission.java
@@ -46,9 +46,10 @@ import javax.xml.bind.annotation.XmlRootElement;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.LinkedHashSet;
+import java.util.Set;
 
 import static java.util.Collections.emptyList;
-import static java.util.Collections.unmodifiableCollection;
+import static java.util.Collections.unmodifiableSet;
 
 //~--- JDK imports ------------------------------------------------------------
 
@@ -67,7 +68,7 @@ public class RepositoryPermission implements PermissionObject, Serializable
   private boolean groupPermission = false;
   private String name;
   @XmlElement(name = "verb")
-  private Collection<String> verbs;
+  private Set<String> verbs;
 
   /**
    * Constructs a new {@link RepositoryPermission}.
@@ -78,7 +79,7 @@ public class RepositoryPermission implements PermissionObject, Serializable
   public RepositoryPermission(String name, Collection<String> verbs, boolean groupPermission)
   {
     this.name = name;
-    this.verbs = unmodifiableCollection(new LinkedHashSet<>(verbs));
+    this.verbs = unmodifiableSet(new LinkedHashSet<>(verbs));
     this.groupPermission = groupPermission;
   }
 
@@ -209,6 +210,6 @@ public class RepositoryPermission implements PermissionObject, Serializable
    */
   public void setVerbs(Collection<String> verbs)
   {
-    this.verbs = verbs;
+    this.verbs = unmodifiableSet(new LinkedHashSet<>(verbs));
   }
 }
