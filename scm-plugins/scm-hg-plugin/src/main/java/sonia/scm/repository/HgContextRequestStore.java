5a5cafaee0ff1e0f5534da54242724f3705235ff
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2019-02-14 11:22:58 +0100

    Add comment from previous commit

--- scm-plugins/scm-hg-plugin/src/main/java/sonia/scm/repository/HgContextRequestStore.java
+++ scm-plugins/scm-hg-plugin/src/main/java/sonia/scm/repository/HgContextRequestStore.java
@@ -4,6 +4,13 @@ import com.google.inject.servlet.RequestScoped;
 
 /**
  * Holds an instance of {@link HgContext} in the request scope.
+ *
+ * <p>The problem seems to be that guice had multiple options for injecting HgContext. {@link HgContextProvider}
+ * bound via Module and {@link HgContext} bound void {@link RequestScoped} annotation. It looks like that Guice 4
+ * injects randomly the one or the other, in SCMv1 (Guice 3) everything works as expected.</p>
+ *
+ * <p>To fix the problem we have created this class annotated with {@link RequestScoped}, which holds an instance
+ * of {@link HgContext}. This way only the {@link HgContextProvider} is used for injection.</p>
  */
 @RequestScoped
 public class HgContextRequestStore {
