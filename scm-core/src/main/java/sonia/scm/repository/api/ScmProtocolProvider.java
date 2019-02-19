1f3c811acf0ff220827f032c444c5f128f3f26b7
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2019-02-19 14:48:38 +0100

    added javadoc

--- scm-core/src/main/java/sonia/scm/repository/api/ScmProtocolProvider.java
+++ scm-core/src/main/java/sonia/scm/repository/api/ScmProtocolProvider.java
@@ -3,10 +3,29 @@ package sonia.scm.repository.api;
 import sonia.scm.plugin.ExtensionPoint;
 import sonia.scm.repository.Repository;
 
+/**
+ * Provider for scm native protocols.
+ *
+ * @param <T> type of protocol
+ *
+ * @since 2.0.0
+ */
 @ExtensionPoint(multi = true)
 public interface ScmProtocolProvider<T extends ScmProtocol> {
 
+  /**
+   * Returns type of repository (e.g.: git, svn, hg, etc.)
+   *
+   * @return name of type
+   */
   String getType();
 
+  /**
+   * Returns protocol for the given repository.
+   *
+   * @param repository repository
+   *
+   * @return protocol for repository
+   */
   T get(Repository repository);
 }
