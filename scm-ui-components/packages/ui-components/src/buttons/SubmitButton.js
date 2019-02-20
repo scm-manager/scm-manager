90cb047ccb08ddffcdeb2e4c8ff5bef891fb5015
Philipp Czora <philipp.czora@cloudogu.com>
2019-02-20 09:47:35 +0100

    SubmitButton: scroll to top after performing action

--- scm-ui-components/packages/ui-components/src/buttons/SubmitButton.js
+++ scm-ui-components/packages/ui-components/src/buttons/SubmitButton.js
@@ -4,12 +4,16 @@ import Button, { type ButtonProps } from "./Button";
 
 class SubmitButton extends React.Component<ButtonProps> {
   render() {
+    const { action } = this.props;
     return (
       <Button
         type="submit"
         color="primary"
         {...this.props}
-        action={() => {
+        action={(event) => {
+          if (action) {
+            action(event)
+          }
           window.scrollTo(0, 0);
         }}
       />
