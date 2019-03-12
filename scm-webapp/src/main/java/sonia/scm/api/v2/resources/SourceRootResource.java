c8b19a9d6625149aeabe42b3907a111cb8c1250b
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2019-03-12 15:44:23 +0100

    Add revision to context

--- scm-webapp/src/main/java/sonia/scm/api/v2/resources/SourceRootResource.java
+++ scm-webapp/src/main/java/sonia/scm/api/v2/resources/SourceRootResource.java
@@ -65,7 +65,7 @@ public class SourceRootResource {
       if (browserResult != null) {
         return Response.ok(browserResultToFileObjectDtoMapper.map(browserResult, namespaceAndName)).build();
       } else {
-        throw notFound(entity("Source", path).in(namespaceAndName));
+        throw notFound(entity("Source", path).in("Revision", revision).in(namespaceAndName));
       }
     }
   }
