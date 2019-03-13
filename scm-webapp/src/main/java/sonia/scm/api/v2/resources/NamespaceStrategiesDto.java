54170c04a7e5e53fb53239b33c5e039bc4974958
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2019-03-13 09:38:27 +0100

    removes unused setter from NamespaceStrategiesDto

--- scm-webapp/src/main/java/sonia/scm/api/v2/resources/NamespaceStrategiesDto.java
+++ scm-webapp/src/main/java/sonia/scm/api/v2/resources/NamespaceStrategiesDto.java
@@ -3,12 +3,10 @@ package sonia.scm.api.v2.resources;
 import de.otto.edison.hal.HalRepresentation;
 import de.otto.edison.hal.Links;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.List;
 
 @Getter
-@Setter
 public class NamespaceStrategiesDto extends HalRepresentation {
 
   private String current;
