e7b7690996572b1be8fb5138adf46adf3ef41a82
Florian Scholdei <florian.scholdei@cloudogu.com>
2019-05-09 16:56:01 +0200

    added placeholder form for create and edit roles

---
+++ scm-ui/src/config/containers/GlobalPermissionRoleForm.js
@@ -0,0 +1,14 @@
+// @flow
+import React from "react";
+
+type Props = {
+  thing?: any,
+};
+
+class GlobalPermissionRoleForm extends React.Component<Props> {
+  render() {
+    return <p>Placeholder</p>;
+  }
+}
+
+export default GlobalPermissionRoleForm;
