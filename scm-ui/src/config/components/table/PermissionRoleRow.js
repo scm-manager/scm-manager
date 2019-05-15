8b4e6ab72d93d1b9f9f1d4da3b931aa7662ef8ca
Florian Scholdei <florian.scholdei@cloudogu.com>
2019-05-15 10:43:13 +0200

    add systemRoleTag for systemRoles

--- scm-ui/src/config/components/table/PermissionRoleRow.js
+++ scm-ui/src/config/components/table/PermissionRoleRow.js
@@ -2,6 +2,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import type { Role } from "@scm-manager/ui-types";
+import SystemRoleTag from "../SystemRoleTag";
 
 type Props = {
   baseUrl: string,
@@ -9,8 +10,15 @@ type Props = {
 };
 
 class PermissionRoleRow extends React.Component<Props> {
-  renderLink(to: string, label: string) {
-    return <Link to={to}>{label}</Link>;
+  renderLink(to: string, label: string, system?: boolean) {
+    if (!system) {
+      return <Link to={to}>{label}</Link>;
+    }
+    return (
+      <>
+        {label} <SystemRoleTag system={system} />
+      </>
+    );
   }
 
   render() {
@@ -18,7 +26,7 @@ class PermissionRoleRow extends React.Component<Props> {
     const to = `${baseUrl}/${encodeURIComponent(role.name)}/edit`;
     return (
       <tr>
-        <td>{this.renderLink(to, role.name)}</td>
+        <td>{this.renderLink(to, role.name, !role._links.update)}</td>
       </tr>
     );
   }
