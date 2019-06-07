70de4d729260ec2d6bbc2c5a4224d602eb1859f0
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2019-06-07 13:59:50 +0200

    Organize imports

--- scm-webapp/src/main/java/sonia/scm/update/MigrationWizardServlet.java
+++ scm-webapp/src/main/java/sonia/scm/update/MigrationWizardServlet.java
@@ -25,8 +25,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static java.util.Arrays.stream;
-
 @Singleton
 class MigrationWizardServlet extends HttpServlet {
 
