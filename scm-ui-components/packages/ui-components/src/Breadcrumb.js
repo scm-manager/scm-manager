3d07b6c12bec14a9d72f78936f00ee5efb959331
Florian Scholdei <florian.scholdei@cloudogu.com>
2019-06-26 15:17:16 +0200

    styling

--- scm-ui-components/packages/ui-components/src/Breadcrumb.js
+++ scm-ui-components/packages/ui-components/src/Breadcrumb.js
@@ -1,44 +1,59 @@
 //@flow
 import React from "react";
 import { Link } from "react-router-dom";
+import injectSheet from "react-jss";
+import classNames from "classnames";
 
 type Props = {
   path: string,
-  baseUrl: string
+  baseUrl: string,
+  classes: any
+};
+
+const styles = {
+  margin: {
+    margin: "1rem 1.25rem 0rem"
+  }
 };
 
 class Breadcrumb extends React.Component<Props> {
   render() {
-    const { path, baseUrl } = this.props;
+    const { path, baseUrl, classes } = this.props;
 
     if (path) {
       const paths = path.split("/");
 
       return (
-        <nav className="breadcrumb" aria-label="breadcrumbs">
-          <ul>
-            {paths.map((path, index) => {
-              if (paths.length - 1 === index) {
+        <>
+          <nav
+            className={classNames("breadcrumb", classes.margin)}
+            aria-label="breadcrumbs"
+          >
+            <ul>
+              {paths.map((path, index) => {
+                if (paths.length - 1 === index) {
+                  return (
+                    <li className="is-active" key={index}>
+                      <Link to={"#"} aria-current="page">
+                        {path}
+                      </Link>
+                    </li>
+                  );
+                }
                 return (
-                  <li className="is-active" key={index}>
-                    <Link to={"#"} aria-current="page">
-                      {path}
-                    </Link>
+                  <li key={index}>
+                    <Link to={baseUrl + "/" + path}>{path}</Link>
                   </li>
                 );
-              }
-              return (
-                <li key={index}>
-                  <Link to={baseUrl + "/" + path}>{path}</Link>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
+              })}
+            </ul>
+          </nav>
+          <hr />
+        </>
       );
     }
     return null;
   }
 }
 
-export default Breadcrumb;
+export default injectSheet(styles)(Breadcrumb);
