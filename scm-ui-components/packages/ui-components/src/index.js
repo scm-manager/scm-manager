23c8ff923efb961ddc4e244b28c66fb3d6be1261
Florian Scholdei <florian.scholdei@cloudogu.com>
2019-07-02 15:36:38 +0200

    exported autocompletes

--- scm-ui-components/packages/ui-components/src/index.js
+++ scm-ui-components/packages/ui-components/src/index.js
@@ -25,6 +25,8 @@ export { default as Tooltip } from "./Tooltip";
 // TODO do we need this? getPageFromMatch is already exported by urls
 export { getPageFromMatch } from "./urls";
 export { default as Autocomplete} from "./Autocomplete";
+export { default as GroupAutocomplete} from "./GroupAutocomplete";
+export { default as UserAutocomplete} from "./UserAutocomplete";
 export { default as BranchSelector } from "./BranchSelector";
 export { default as MarkdownView } from "./MarkdownView";
 export { default as SyntaxHighlighter } from "./SyntaxHighlighter";
