1ea2bdfedff9ad540de50dc302aa2264d1b63fee
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2019-07-25 11:34:16 +0200

    change type to category

--- scm-webapp/src/main/java/sonia/scm/api/v2/resources/PluginDto.java
+++ scm-webapp/src/main/java/sonia/scm/api/v2/resources/PluginDto.java
@@ -13,7 +13,7 @@ import lombok.Setter;
 public class PluginDto extends HalRepresentation {
 
   private String name;
-  private String type;
+  private String category;
   private String version;
   private String author;
   private String description;
