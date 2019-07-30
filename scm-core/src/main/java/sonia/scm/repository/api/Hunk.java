0b76cb7ea5b5af07544b76d37f2df4d9e84f2547
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2019-07-30 10:00:36 +0200

    added raw header to hunk

--- scm-core/src/main/java/sonia/scm/repository/api/Hunk.java
+++ scm-core/src/main/java/sonia/scm/repository/api/Hunk.java
@@ -2,6 +2,18 @@ package sonia.scm.repository.api;
 
 public interface Hunk extends Iterable<DiffLine> {
 
+  default String getRawHeader() {
+    return String.format("@@ -%s +%s @@", getLineMarker(getOldStart(), getOldLineCount()), getLineMarker(getNewStart(), getNewLineCount()));
+  }
+
+  default String getLineMarker(int start, int lineCount) {
+    if (lineCount == 1) {
+      return Integer.toString(start);
+    } else {
+      return String.format("%s,%s", start, lineCount);
+    }
+  }
+
   int getOldStart();
 
   int getOldLineCount();
