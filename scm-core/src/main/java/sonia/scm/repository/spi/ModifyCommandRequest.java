695457e9491530df3e18276a5e0fca36652fa4b0
Rene Pfeuffer <rene.pfeuffer@cloudogu.com>
2019-09-03 08:32:07 +0200

    Make branch optional in modify command
    Due to the fact that the default branch is configured in HEAD, it will
    be checked out by default by the clone in GitModifyCommand. Therefore
    we won't have to do anything to "select" the default branch if no other
    branch is specified.

--- scm-core/src/main/java/sonia/scm/repository/spi/ModifyCommandRequest.java
+++ scm-core/src/main/java/sonia/scm/repository/spi/ModifyCommandRequest.java
@@ -65,7 +65,7 @@ public class ModifyCommandRequest implements Resetable, Validateable {
 
   @Override
   public boolean isValid() {
-    return StringUtils.isNotEmpty(commitMessage) && StringUtils.isNotEmpty(branch) && !requests.isEmpty();
+    return StringUtils.isNotEmpty(commitMessage) && !requests.isEmpty();
   }
 
   public interface PartialRequest {
