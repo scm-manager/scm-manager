fb5617d94048ce26439972507c4e03fd8c680801
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2019-10-01 14:41:56 +0200

    implement create method for HgModifyCommand

--- scm-plugins/scm-hg-plugin/src/main/java/sonia/scm/repository/spi/HgModifyCommand.java
+++ scm-plugins/scm-hg-plugin/src/main/java/sonia/scm/repository/spi/HgModifyCommand.java
@@ -5,12 +5,22 @@ import com.aragost.javahg.Repository;
 import com.aragost.javahg.commands.CommitCommand;
 import com.aragost.javahg.commands.PushCommand;
 import com.aragost.javahg.commands.RemoveCommand;
+import org.apache.commons.lang.StringUtils;
+import sonia.scm.ContextEntry;
 import sonia.scm.repository.HgRepositoryHandler;
+import sonia.scm.repository.InternalRepositoryException;
 import sonia.scm.repository.util.WorkingCopy;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import static sonia.scm.AlreadyExistsException.alreadyExists;
+import static sonia.scm.ContextEntry.ContextBuilder.entity;
 
 public class HgModifyCommand implements ModifyCommand {
 
@@ -39,8 +49,22 @@ public class HgModifyCommand implements ModifyCommand {
               }
 
               @Override
-              public void create(String toBeCreated, File file, boolean overwrite) {
-
+              public void create(String toBeCreated, File file, boolean overwrite) throws IOException {
+                Path targetFile = new File(workingRepository.getDirectory(), toBeCreated).toPath();
+                if (overwrite) {
+                  Files.move(file.toPath(), targetFile, REPLACE_EXISTING);
+                } else {
+                  try {
+                    Files.move(file.toPath(), targetFile);
+                  } catch (FileAlreadyExistsException e) {
+                    throw alreadyExists(createFileContext(toBeCreated));
+                  }
+                }
+                try {
+                  addFileToHg(targetFile.toFile());
+                } catch (Exception e) {
+                  throwInternalRepositoryException("could not add new file to index", e);
+                }
               }
 
               @Override
@@ -52,6 +76,28 @@ public class HgModifyCommand implements ModifyCommand {
               public void move(String sourcePath, String targetPath) {
 
               }
+
+              private void createDirectories(Path targetFile) throws IOException {
+                try {
+                  Files.createDirectories(targetFile.getParent());
+                } catch (FileAlreadyExistsException e) {
+                  throw alreadyExists(createFileContext(targetFile.toString()));
+                }
+              }
+
+              private ContextEntry.ContextBuilder createFileContext(String path) {
+                ContextEntry.ContextBuilder contextBuilder = entity("file", path);
+                if (!StringUtils.isEmpty(request.getBranch())) {
+                  contextBuilder.in("branch", request.getBranch());
+                }
+                contextBuilder.in(context.getScmRepository());
+                return contextBuilder;
+              }
+
+              private void addFileToHg(File file) {
+                workingRepository.workingCopy().add(file.getAbsolutePath());
+              }
+
             });
           } catch (IOException e) {
             e.printStackTrace(); // TODO
@@ -67,6 +113,9 @@ public class HgModifyCommand implements ModifyCommand {
       e.printStackTrace();
       return null;
     }
+  }
 
+  private String throwInternalRepositoryException(String message, Exception e) {
+    throw new InternalRepositoryException(context.getScmRepository(), message, e);
   }
 }
--- scm-plugins/scm-hg-plugin/src/test/java/sonia/scm/repository/spi/HgModifyCommandTest.java
+++ scm-plugins/scm-hg-plugin/src/test/java/sonia/scm/repository/spi/HgModifyCommandTest.java
@@ -1,12 +1,19 @@
 package sonia.scm.repository.spi;
 
 import com.google.inject.util.Providers;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import sonia.scm.AlreadyExistsException;
 import sonia.scm.repository.HgHookManager;
 import sonia.scm.repository.Person;
 import sonia.scm.repository.util.WorkdirProvider;
 import sonia.scm.web.HgRepositoryEnvironmentBuilder;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,15 +22,28 @@ import static org.mockito.Mockito.when;
 
 public class HgModifyCommandTest extends AbstractHgCommandTestBase {
 
-  @Test
-  public void shouldRemoveFiles() throws IOException {
+  private HgModifyCommand hgModifyCommand;
+
+  @Rule
+  public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+  @Before
+  public void initHgModifyCommand() {
     HgHookManager hookManager = mock(HgHookManager.class);
     when(hookManager.getChallenge()).thenReturn("CHALLENGE");
     when(hookManager.getCredentials()).thenReturn("SECRET:SECRET");
     when(hookManager.createUrl()).thenReturn("http://localhost");
     HgRepositoryEnvironmentBuilder environmentBuilder = new HgRepositoryEnvironmentBuilder(handler, hookManager);
+    hgModifyCommand = new HgModifyCommand(handler, cmdContext, new SimpleHgWorkdirFactory(Providers.of(environmentBuilder), new WorkdirProvider()));
+  }
 
-    HgModifyCommand hgModifyCommand = new HgModifyCommand(handler, cmdContext, new SimpleHgWorkdirFactory(Providers.of(environmentBuilder), new WorkdirProvider()));
+  @After
+  public void closeRepository() throws IOException {
+    cmdContext.close();
+  }
+
+  @Test
+  public void shouldRemoveFiles() {
     ModifyCommandRequest request = new ModifyCommandRequest();
     request.addRequest(new ModifyCommandRequest.DeleteFileRequest("a.txt"));
     request.setCommitMessage("this is great");
@@ -32,6 +52,69 @@ public class HgModifyCommandTest extends AbstractHgCommandTestBase {
     String result = hgModifyCommand.execute(request);
 
     assertThat(cmdContext.open().tip().getNode()).isEqualTo(result);
-    cmdContext.close();
+  }
+
+  @Test
+  public void shouldCreateFilesWithoutOverwrite() throws IOException {
+
+    File testFile = temporaryFolder.newFile("Answer.txt");
+
+    ModifyCommandRequest request = new ModifyCommandRequest();
+    request.addRequest(new ModifyCommandRequest.CreateFileRequest("Answer.txt", testFile, false));
+    request.setCommitMessage("I found the answer");
+    request.setAuthor(new Person("Trillian Astra", "trillian@hitchhiker.com"));
+
+    String changeSet = hgModifyCommand.execute(request);
+
+    assertThat(cmdContext.open().tip().getNode()).isEqualTo(changeSet);
+    assertThat(cmdContext.open().tip().getAddedFiles().size()).isEqualTo(1);
+  }
+
+  @Test
+  public void shouldOverwriteExistingFiles() throws IOException {
+
+    File testFile = temporaryFolder.newFile("Answer.txt");
+    new FileOutputStream(testFile).write(21);
+
+    ModifyCommandRequest request = new ModifyCommandRequest();
+    request.addRequest(new ModifyCommandRequest.CreateFileRequest("Answer.txt", testFile, false));
+    request.setCommitMessage("I found the answer");
+    request.setAuthor(new Person("Trillian Astra", "trillian@hitchhiker.com"));
+
+    hgModifyCommand.execute(request);
+
+    new FileOutputStream(testFile).write(42);
+    ModifyCommandRequest request2 = new ModifyCommandRequest();
+    request2.addRequest(new ModifyCommandRequest.CreateFileRequest("Answer.txt", testFile, true));
+    request2.setCommitMessage(" Now i really found the answer");
+    request2.setAuthor(new Person("Trillian Astra", "trillian@hitchhiker.com"));
+
+    String changeSet2 = hgModifyCommand.execute(request2);
+
+    assertThat(cmdContext.open().tip().getNode()).isEqualTo(changeSet2);
+    assertThat(cmdContext.open().tip().getModifiedFiles().size()).isEqualTo(1);
+    assertThat(cmdContext.open().tip().getModifiedFiles().get(0)).isEqualTo(testFile.getName());
+  }
+
+  @Test(expected = AlreadyExistsException.class)
+  public void shouldThrowFileAlreadyExistsException() throws IOException {
+
+    File testFile = temporaryFolder.newFile("Answer.txt");
+    new FileOutputStream(testFile).write(21);
+
+    ModifyCommandRequest request = new ModifyCommandRequest();
+    request.addRequest(new ModifyCommandRequest.CreateFileRequest("Answer.txt", testFile, false));
+    request.setCommitMessage("I found the answer");
+    request.setAuthor(new Person("Trillian Astra", "trillian@hitchhiker.com"));
+
+    hgModifyCommand.execute(request);
+
+    new FileOutputStream(testFile).write(42);
+    ModifyCommandRequest request2 = new ModifyCommandRequest();
+    request2.addRequest(new ModifyCommandRequest.CreateFileRequest("Answer.txt", testFile, false));
+    request2.setCommitMessage(" Now i really found the answer");
+    request2.setAuthor(new Person("Trillian Astra", "trillian@hitchhiker.com"));
+
+    hgModifyCommand.execute(request2);
   }
 }
