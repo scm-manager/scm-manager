2c93b8362f5d0abaf3b1ad6207715616fcbf5d18
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2019-10-20 18:11:43 +0200

    added typscript support to eslint-config

--- scm-ui/eslint-config/index.js
+++ scm-ui/eslint-config/index.js
@@ -9,5 +9,21 @@ module.exports = {
     "jsx-a11y/href-no-hash": [0],
     "flowtype/no-types-missing-file-annotation": 2,
     "no-console": "error"
-  }
+  },
+  "overrides": [
+    {
+      files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
+      "extends": [
+        "react-app",
+        "plugin:@typescript-eslint/recommended"
+      ],
+      "rules": {
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"],
+        "jsx-a11y/href-no-hash": [0],
+        "no-console": "error"
+      }
+    }
+  ]
 };
