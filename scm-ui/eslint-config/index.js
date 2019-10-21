bcca69156150bfe075f07ed0831d83808dfbd85c
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2019-10-21 10:49:51 +0200

    adjust eslint rules

--- scm-ui/eslint-config/index.js
+++ scm-ui/eslint-config/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   "extends": [
     "react-app",
+    "plugin:prettier/recommended",
     "plugin:flowtype/recommended"
   ],
   "rules": {
@@ -16,12 +17,14 @@ module.exports = {
       parser: "@typescript-eslint/parser",
       "extends": [
         "react-app",
+        "plugin:prettier/recommended",
         "plugin:@typescript-eslint/recommended"
       ],
       "rules": {
         "semi": ["error", "always"],
         "quotes": ["error", "double"],
         "jsx-a11y/href-no-hash": [0],
+        "@typescript-eslint/explicit-function-return-type": "off",
         "no-console": "error"
       }
     }
