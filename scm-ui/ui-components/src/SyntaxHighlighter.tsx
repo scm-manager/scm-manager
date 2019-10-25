162dbc421fb9dadbde2a6fb3d77ae4b5ddff8ed2
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2019-10-25 15:06:34 +0200

    fix markdown code block without language attribute

--- scm-ui/ui-components/src/SyntaxHighlighter.tsx
+++ scm-ui/ui-components/src/SyntaxHighlighter.tsx
@@ -5,14 +5,29 @@ import { LightAsync as ReactSyntaxHighlighter } from "react-syntax-highlighter";
 import { arduinoLight } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
 type Props = {
-  language: string;
+  language?: string;
   value: string;
 };
 
+const defaultLanguage = "text";
+
 class SyntaxHighlighter extends React.Component<Props> {
+  static defaultProps: Partial<Props> = {
+    language: defaultLanguage
+  };
+
+  getLanguage = () => {
+    const { language } = this.props;
+    if (language) {
+      return language;
+    }
+    return defaultLanguage;
+  };
+
   render() {
+    const language = this.getLanguage();
     return (
-      <ReactSyntaxHighlighter showLineNumbers={false} language={this.props.language} style={arduinoLight}>
+      <ReactSyntaxHighlighter showLineNumbers={false} language={language} style={arduinoLight}>
         {this.props.value}
       </ReactSyntaxHighlighter>
     );
