2f48087f1390d7ca2e674a113797f7f2fd48013c
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2020-01-13 21:38:28 +0100

    enable react fast refresh for core ui

--- scm-ui/babel-preset/index.js
+++ scm-ui/babel-preset/index.js
@@ -1,12 +1,11 @@
-module.exports = () => ({
-  presets: [
-    require("@babel/preset-env"),
-    require("@babel/preset-flow"),
-    require("@babel/preset-react"),
-    require("@babel/preset-typescript")
-  ],
-  plugins: [
-    require("@babel/plugin-proposal-class-properties"),
-    require("@babel/plugin-proposal-optional-chaining")
-  ]
-});
+module.exports = api => {
+  return {
+    presets: [
+      require("@babel/preset-env"),
+      require("@babel/preset-flow"),
+      require("@babel/preset-react"),
+      require("@babel/preset-typescript")
+    ],
+    plugins: [require("@babel/plugin-proposal-class-properties"), require("@babel/plugin-proposal-optional-chaining")]
+  };
+};
