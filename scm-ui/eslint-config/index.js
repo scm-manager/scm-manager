271d8ad3d9b60b09b647af7406d910ae29bbf671
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2020-06-15 20:32:28 +0200

    remove flow based configuration and use ts as default

--- scm-ui/eslint-config/index.js
+++ scm-ui/eslint-config/index.js
@@ -22,35 +22,15 @@
  * SOFTWARE.
  */
 module.exports = {
-  extends: [
-    "react-app",
-    "plugin:prettier/recommended",
-    "plugin:flowtype/recommended"
-  ],
+  parser: "@typescript-eslint/parser",
+  extends: ["react-app", "plugin:prettier/recommended", "plugin:@typescript-eslint/recommended"],
   rules: {
+    "prettier/prettier": "warn",
     semi: ["error", "always"],
     quotes: ["error", "double"],
     "jsx-a11y/href-no-hash": [0],
-    "flowtype/no-types-missing-file-annotation": 2,
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/ban-ts-ignore": "warn",
     "no-console": "error"
-  },
-  overrides: [
-    {
-      files: ["*.ts", "*.tsx"],
-      parser: "@typescript-eslint/parser",
-      extends: [
-        "react-app",
-        "plugin:prettier/recommended",
-        "plugin:@typescript-eslint/recommended"
-      ],
-      rules: {
-        semi: ["error", "always"],
-        quotes: ["error", "double"],
-        "jsx-a11y/href-no-hash": [0],
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/ban-ts-ignore": "warn",
-        "no-console": "error"
-      }
-    }
-  ]
+  }
 };
