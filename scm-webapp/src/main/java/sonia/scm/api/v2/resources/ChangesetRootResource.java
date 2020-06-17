c16c6e006e9da3f8fdc2c29baa6f67c4a62a41ae
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2020-06-17 17:25:41 +0200

    removes unused import

--- scm-webapp/src/main/java/sonia/scm/api/v2/resources/ChangesetRootResource.java
+++ scm-webapp/src/main/java/sonia/scm/api/v2/resources/ChangesetRootResource.java
@@ -48,7 +48,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
-import java.util.Optional;
 
 import static sonia.scm.ContextEntry.ContextBuilder.entity;
 import static sonia.scm.NotFoundException.notFound;
