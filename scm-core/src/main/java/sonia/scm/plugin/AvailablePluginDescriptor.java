38047a25aa465039bddc5bda0424133dfa29e6b3
Konstantin Schaper <konstantin.schaper@cloudogu.com>
2020-07-21 11:39:08 +0200

    Add optional dependencies to plugin descriptors and plugin center dto

--- scm-core/src/main/java/sonia/scm/plugin/AvailablePluginDescriptor.java
+++ scm-core/src/main/java/sonia/scm/plugin/AvailablePluginDescriptor.java
@@ -21,11 +21,13 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-    
+
 package sonia.scm.plugin;
 
 import java.util.Optional;
 import java.util.Set;
+
+import static java.util.Collections.emptySet;
 
 /**
  * @since 2.0.0
@@ -35,13 +37,23 @@ public class AvailablePluginDescriptor implements PluginDescriptor {
   private final PluginInformation information;
   private final PluginCondition condition;
   private final Set<String> dependencies;
+  private final Set<String> optionalDependencies;
   private final String url;
   private final String checksum;
 
+  /**
+   * @deprecated Use {@link #AvailablePluginDescriptor(PluginInformation, PluginCondition, Set, Set, String, String)} instead
+   */
+  @Deprecated
   public AvailablePluginDescriptor(PluginInformation information, PluginCondition condition, Set<String> dependencies, String url, String checksum) {
+    this(information, condition, dependencies, emptySet(), url, checksum);
+  }
+
+  public AvailablePluginDescriptor(PluginInformation information, PluginCondition condition, Set<String> dependencies, Set<String> optionalDependencies, String url, String checksum) {
     this.information = information;
     this.condition = condition;
     this.dependencies = dependencies;
+    this.optionalDependencies = optionalDependencies;
     this.url = url;
     this.checksum = checksum;
   }
@@ -67,5 +79,10 @@ public class AvailablePluginDescriptor implements PluginDescriptor {
   @Override
   public Set<String> getDependencies() {
     return dependencies;
+  }
+
+  @Override
+  public Set<String> getOptionalDependencies() {
+    return optionalDependencies;
   }
 }
