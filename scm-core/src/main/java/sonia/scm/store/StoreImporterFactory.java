6b91280d2290330906899b270972a292d9c71f0e
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2021-01-13 09:46:27 +0100

    create store importer api

---
+++ scm-core/src/main/java/sonia/scm/store/StoreImporterFactory.java
@@ -0,0 +1,42 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020-present Cloudogu GmbH and Contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package sonia.scm.store;
+
+import sonia.scm.repository.Repository;
+
+/**
+ * The {@link StoreImporterFactory} is used to create a {@link StoreEntryImporterFactory} for a {@link Repository}.
+ *
+ * @since 2.13.0
+ */
+public interface StoreImporterFactory {
+  /**
+   * Returns a {@link StoreEntryImporterFactory} for the {@link Repository}
+   *
+   * @param repository
+   * @return {@link StoreEntryImporterFactory}
+   */
+  StoreEntryImporterFactory importer(Repository repository);
+}
