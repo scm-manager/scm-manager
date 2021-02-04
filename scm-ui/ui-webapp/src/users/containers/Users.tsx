674fcda6a47032f7beca8e7bbbd8c03697642de4
Konstantin Schaper <konstantin.schaper@cloudogu.com>
2021-02-04 15:52:35 +0100

    implement swr for users and profile

--- scm-ui/ui-webapp/src/users/containers/Users.tsx
+++ scm-ui/ui-webapp/src/users/containers/Users.tsx
@@ -21,11 +21,9 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-import React from "react";
-import { connect } from "react-redux";
-import { WithTranslation, withTranslation } from "react-i18next";
-import { RouteComponentProps } from "react-router-dom";
-import { PagedCollection, User } from "@scm-manager/ui-types";
+import React, { FC } from "react";
+import { useTranslation } from "react-i18next";
+import { useLocation, useParams } from "react-router-dom";
 import {
   CreateButton,
   LinkPaginator,
@@ -35,124 +33,57 @@ import {
   PageActions,
   urls
 } from "@scm-manager/ui-components";
-import { mustGetUsersLink } from "../../modules/indexResource";
-import {
-  fetchUsersByPage,
-  getFetchUsersFailure,
-  getUsersFromState,
-  isFetchUsersPending,
-  isPermittedToCreateUsers,
-  selectListAsCollection
-} from "../modules/users";
 import { UserTable } from "./../components/table";
+import { useUsers } from "@scm-manager/ui-api";
 
-type Props = RouteComponentProps &
-  WithTranslation & {
-    users: User[];
-    loading: boolean;
-    error: Error;
-    canAddUsers: boolean;
-    list: PagedCollection;
-    page: number;
-    usersLink: string;
+const Users: FC = () => {
+  const location = useLocation();
+  const params = useParams();
+  const search = urls.getQueryStringFromLocation(location);
+  const page = urls.getPageFromMatch({ params });
+  const { isLoading, error, data } = useUsers({ page: page - 1, search });
+  const users = data?._embedded.users;
+  const [t] = useTranslation("users");
+  const canAddUsers = !!data?._links.create;
 
-    // dispatch functions
-    fetchUsersByPage: (link: string, page: number, filter?: string) => void;
-  };
-
-class Users extends React.Component<Props> {
-  componentDidMount() {
-    const { fetchUsersByPage, usersLink, page, location } = this.props;
-    fetchUsersByPage(usersLink, page, urls.getQueryStringFromLocation(location));
-  }
-
-  componentDidUpdate = (prevProps: Props) => {
-    const { loading, list, page, usersLink, location, fetchUsersByPage } = this.props;
-    if (list && page && !loading) {
-      const statePage: number = this.resolveStatePage();
-      if (page !== statePage || prevProps.location.search !== location.search) {
-        fetchUsersByPage(usersLink, page, urls.getQueryStringFromLocation(location));
-      }
-    }
-  };
-
-  resolveStatePage = (props = this.props) => {
-    const { list } = props;
-    if (list.page) {
-      return list.page + 1;
-    }
-    // set page to 1 if undefined, because if users couldn't be fetched it would lead to a fetch-loop otherwise
-    return 1;
-  };
-
-  render() {
-    const { users, loading, error, canAddUsers, t } = this.props;
-
-    return (
-      <Page title={t("users.title")} subtitle={t("users.subtitle")} loading={loading || !users} error={error}>
-        {this.renderUserTable()}
-        {this.renderCreateButton()}
-        <PageActions>
-          <OverviewPageActions
-            showCreateButton={canAddUsers}
-            link="users"
-            label={t("users.createButton")}
-            searchPlaceholder={t("overview.searchUser")}
-          />
-        </PageActions>
-      </Page>
-    );
-  }
-
-  renderUserTable() {
-    const { users, list, page, location, t } = this.props;
-    if (users && users.length > 0) {
+  const renderUserTable = () => {
+    if (data && users && users.length > 0) {
       return (
         <>
           <UserTable users={users} />
-          <LinkPaginator collection={list} page={page} filter={urls.getQueryStringFromLocation(location)} />
+          <LinkPaginator collection={data} page={page} filter={urls.getQueryStringFromLocation(location)} />
         </>
       );
     }
     return <Notification type="info">{t("users.noUsers")}</Notification>;
-  }
+  };
 
-  renderCreateButton() {
-    const { canAddUsers, t } = this.props;
+  const renderCreateButton = () => {
     if (canAddUsers) {
       return <CreateButton label={t("users.createButton")} link="/users/create" />;
     }
     return null;
-  }
-}
-
-const mapStateToProps = (state: any, ownProps: Props) => {
-  const { match } = ownProps;
-  const users = getUsersFromState(state);
-  const loading = isFetchUsersPending(state);
-  const error = getFetchUsersFailure(state);
-  const page = urls.getPageFromMatch(match);
-  const canAddUsers = isPermittedToCreateUsers(state);
-  const list = selectListAsCollection(state);
-  const usersLink = mustGetUsersLink(state);
-
-  return {
-    users,
-    loading,
-    error,
-    canAddUsers,
-    list,
-    page,
-    usersLink
   };
+
+  return (
+    <Page
+      title={t("users.title")}
+      subtitle={t("users.subtitle")}
+      loading={isLoading || !users}
+      error={error || undefined}
+    >
+      {renderUserTable()}
+      {renderCreateButton()}
+      <PageActions>
+        <OverviewPageActions
+          showCreateButton={canAddUsers}
+          link="users"
+          label={t("users.createButton")}
+          searchPlaceholder={t("overview.searchUser")}
+        />
+      </PageActions>
+    </Page>
+  );
 };
 
-const mapDispatchToProps = (dispatch: any) => {
-  return {
-    fetchUsersByPage: (link: string, page: number, filter?: string) => {
-      dispatch(fetchUsersByPage(link, page, filter));
-    }
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(withTranslation("users")(Users));
+export default Users;
