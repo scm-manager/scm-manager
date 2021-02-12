457e8b9fa89c71c0fdfecb3fc134b192b466a636
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2021-02-12 10:41:04 +0100

    Use ui-api for repository and namespace permissions

--- scm-ui/ui-types/src/repositoryVerbs.ts
+++ scm-ui/ui-types/src/repositoryVerbs.ts
@@ -20,22 +20,11 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
+ *
  */
 
-import { validation } from "@scm-manager/ui-components";
-import { PermissionCollection } from "@scm-manager/ui-types";
+import { HalRepresentation } from "./hal";
 
-const isNameValid = validation.isNameValid;
-
-export { isNameValid };
-
-export const isPermissionValid = (name: string, groupPermission: boolean, permissions: PermissionCollection) => {
-  return isNameValid(name) && !currentPermissionIncludeName(name, groupPermission, permissions);
-};
-
-const currentPermissionIncludeName = (name: string, groupPermission: boolean, permissions: PermissionCollection) => {
-  for (let i = 0; i < permissions.length; i++) {
-    if (permissions[i].name === name && permissions[i].groupPermission === groupPermission) return true;
-  }
-  return false;
+export type RepositoryVerbs = HalRepresentation & {
+  verbs: string[];
 };
