15e4f7c5d9bb059b45ff9915e138b79a76643f43
Konstantin Schaper <konstantin.schaper@cloudogu.com>
2021-02-15 08:33:08 +0100

    wip plugins rework

--- scm-ui/ui-webapp/src/admin/plugins/components/ExecutePendingActionModal.tsx
+++ scm-ui/ui-webapp/src/admin/plugins/components/ExecutePendingActionModal.tsx
@@ -21,39 +21,36 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-import React from "react";
-import { WithTranslation, withTranslation } from "react-i18next";
+import React, { FC } from "react";
+import { useTranslation } from "react-i18next";
 import { PendingPlugins } from "@scm-manager/ui-types";
-import { apiClient, Notification } from "@scm-manager/ui-components";
-import waitForRestart from "./waitForRestart";
+import { Notification } from "@scm-manager/ui-components";
 import PluginActionModal from "./PluginActionModal";
+import { useExecutePendingPlugins } from "@scm-manager/ui-api";
 
-type Props = WithTranslation & {
+type Props = {
   onClose: () => void;
   pendingPlugins: PendingPlugins;
 };
 
-class ExecutePendingActionModal extends React.Component<Props> {
-  render() {
-    const { onClose, pendingPlugins, t } = this.props;
+const ExecutePendingActionModal: FC<Props> = ({ onClose, pendingPlugins }) => {
+  const [t] = useTranslation("admin");
+  const { update, isLoading, error, isExecuted } = useExecutePendingPlugins();
 
-    return (
-      <PluginActionModal
-        description={t("plugins.modal.executePending")}
-        label={t("plugins.modal.executeAndRestart")}
-        onClose={onClose}
-        pendingPlugins={pendingPlugins}
-        execute={this.executeAndRestart}
-      >
-        <Notification type="warning">{t("plugins.modal.restartNotification")}</Notification>
-      </PluginActionModal>
-    );
-  }
+  return (
+    <PluginActionModal
+      description={t("plugins.modal.executePending")}
+      label={t("plugins.modal.executeAndRestart")}
+      onClose={onClose}
+      pendingPlugins={pendingPlugins}
+      error={error}
+      loading={isLoading}
+      success={isExecuted}
+      execute={() => update(pendingPlugins)}
+    >
+      <Notification type="warning">{t("plugins.modal.restartNotification")}</Notification>
+    </PluginActionModal>
+  );
+};
 
-  executeAndRestart = () => {
-    const { pendingPlugins } = this.props;
-    return apiClient.post(pendingPlugins._links.execute.href).then(waitForRestart);
-  };
-}
-
-export default withTranslation("admin")(ExecutePendingActionModal);
+export default ExecutePendingActionModal;
