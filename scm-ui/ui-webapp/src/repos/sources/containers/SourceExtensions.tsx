2024d494ac53f2475ccf66f9ac96d9a0206fcc2a
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2021-02-17 16:52:01 +0100

    Implement truncated and partial source results

--- scm-ui/ui-webapp/src/repos/sources/containers/SourceExtensions.tsx
+++ scm-ui/ui-webapp/src/repos/sources/containers/SourceExtensions.tsx
@@ -21,91 +21,55 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-import React from "react";
-import { File, Repository } from "@scm-manager/ui-types";
-import { RouteComponentProps, withRouter } from "react-router-dom";
+import React, { FC } from "react";
+import { Repository } from "@scm-manager/ui-types";
+import { useParams } from "react-router-dom";
 
 import { binder, ExtensionPoint } from "@scm-manager/ui-extensions";
-import { fetchSources, getFetchSourcesFailure, getSources, isFetchSourcesPending } from "../modules/sources";
-import { connect } from "react-redux";
 import { ErrorNotification, Loading, Notification } from "@scm-manager/ui-components";
-import { WithTranslation, withTranslation } from "react-i18next";
-
-type Props = WithTranslation &
-  RouteComponentProps & {
-    repository: Repository;
-    baseUrl: string;
-
-    // url params
-    extension: string;
-    revision?: string;
-    path?: string;
-
-    // redux state
-    loading: boolean;
-    error?: Error | null;
-    sources?: File | null;
-
-    // dispatch props
-    fetchSources: (repository: Repository, revision?: string, path?: string) => void;
-  };
+import { useTranslation } from "react-i18next";
+import { useSources } from "@scm-manager/ui-api";
 
 const extensionPointName = "repos.sources.extensions";
 
-class SourceExtensions extends React.Component<Props> {
-  componentDidMount() {
-    const { fetchSources, repository, revision, path } = this.props;
-    // TODO get typing right
-    fetchSources(repository, revision, path);
-  }
+type Props = {
+  repository: Repository;
+  baseUrl: string;
+};
 
-  render() {
-    const { loading, error, repository, extension, revision, path, sources, baseUrl, t } = this.props;
-    if (error) {
-      return <ErrorNotification error={error} />;
-    }
-    if (loading) {
-      return <Loading />;
-    }
+type Params = {
+  revision: string;
+  path: string;
+  extension: string;
+};
 
-    const extprops = { extension, repository, revision, path, sources, baseUrl };
-    if (!binder.hasExtension(extensionPointName, extprops)) {
-      return <Notification type="warning">{t("sources.extension.notBound")}</Notification>;
-    }
-
-    return <ExtensionPoint name={extensionPointName} props={extprops} />;
-  }
-}
-
-const mapStateToProps = (state: any, ownProps: Props): Partial<Props> => {
-  const { repository, match } = ownProps;
-  // @ts-ignore
-  const revision: string = match.params.revision;
-  // @ts-ignore
-  const path: string = match.params.path;
-  // @ts-ignore
-  const extension: string = match.params.extension;
-  const loading = isFetchSourcesPending(state, repository, revision, path);
-  const error = getFetchSourcesFailure(state, repository, revision, path);
-  const sources = getSources(state, repository, revision, path);
-
+const useUrlParams = () => {
+  const { revision, path, extension } = useParams<Params>();
   return {
-    repository,
-    extension,
-    revision,
-    path,
-    loading,
-    error,
-    sources
+    revision: revision ? decodeURIComponent(revision) : undefined,
+    path: path,
+    extension
   };
 };
 
-const mapDispatchToProps = (dispatch: any) => {
-  return {
-    fetchSources: (repository: Repository, revision: string, path: string) => {
-      dispatch(fetchSources(repository, decodeURIComponent(revision), path));
-    }
-  };
+const SourceExtensions: FC<Props> = ({ repository, baseUrl }) => {
+  const { revision, path, extension } = useUrlParams();
+  const { error, isLoading, data: sources } = useSources(repository, { revision, path });
+  const [t] = useTranslation("repos");
+
+  if (error) {
+    return <ErrorNotification error={error} />;
+  }
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  const extprops = { extension, repository, revision, path, sources, baseUrl };
+  if (!binder.hasExtension(extensionPointName, extprops)) {
+    return <Notification type="warning">{t("sources.extension.notBound")}</Notification>;
+  }
+
+  return <ExtensionPoint name={extensionPointName} props={extprops} />;
 };
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(withTranslation("repos")(SourceExtensions)));
+export default SourceExtensions;
