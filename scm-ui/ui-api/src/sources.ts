50fcc4027a3bc560e5bfc5119cc1ceac14f55132
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2021-02-17 16:52:01 +0100

    Implement truncated and partial source results

--- scm-ui/ui-api/src/sources.ts
+++ scm-ui/ui-api/src/sources.ts
@@ -23,20 +23,69 @@
  *
  */
 
-import { File, Repository } from "@scm-manager/ui-types";
+import { File, Link, Repository } from "@scm-manager/ui-types";
 import { requiredLink } from "./links";
 import { apiClient, urls } from "@scm-manager/ui-components";
-import { useQuery } from "react-query";
-import { ApiResult } from "./base";
+import { useInfiniteQuery } from "react-query";
 import { repoQueryKey } from "./keys";
+import { useEffect } from "react";
 
 export type UseSourcesOptions = {
   revision?: string;
   path?: string;
-  enabled: boolean;
+  refetchPartialInterval?: number;
+  enabled?: boolean;
 };
 
-export const useSources = (repository: Repository, options: UseSourcesOptions = { enabled: true }): ApiResult<File> => {
+const UseSourcesDefaultOptions: UseSourcesOptions = {
+  enabled: true,
+  refetchPartialInterval: 3000
+};
+
+export const useSources = (repository: Repository, opts: UseSourcesOptions = UseSourcesDefaultOptions) => {
+  const options = {
+    ...UseSourcesDefaultOptions,
+    ...opts
+  };
+  const link = createSourcesLink(repository, options);
+  const { isLoading, error, data, isFetchingNextPage, fetchNextPage, refetch } = useInfiniteQuery<File, Error, File>(
+    repoQueryKey(repository, "sources", options.revision || "", options.path || ""),
+    ({ pageParam }) => {
+      return apiClient.get(pageParam || link).then(response => response.json());
+    },
+    {
+      enabled: options.enabled,
+      getNextPageParam: lastPage => {
+        return (lastPage._links.proceed as Link)?.href;
+      }
+    }
+  );
+
+  const file = merge(data?.pages);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (isPartial(file)) {
+        refetch({
+          throwOnError: true
+        });
+      }
+    }, options.refetchPartialInterval);
+    return () => clearInterval(intervalId);
+  }, [options.refetchPartialInterval, file]);
+
+  return {
+    isLoading,
+    error,
+    data: file,
+    isFetchingNextPage,
+    fetchNextPage: () => {
+      // wrapped because we do not want to leak react-query types in our api
+      fetchNextPage();
+    }
+  };
+};
+
+const createSourcesLink = (repository: Repository, options: UseSourcesOptions) => {
   let link = requiredLink(repository, "sources");
   if (options.revision) {
     link = urls.concat(link, encodeURIComponent(options.revision));
@@ -45,11 +94,38 @@ export const useSources = (repository: Repository, options: UseSourcesOptions = 
       link = urls.concat(link, options.path);
     }
   }
-  return useQuery<File, Error>(
-    repoQueryKey(repository, "sources", options.revision || "", options.path || ""),
-    () => apiClient.get(link).then(response => response.json()),
-    {
-      enabled: options.enabled
+  return link;
+};
+
+const merge = (files?: File[]): File | undefined => {
+  if (!files || files.length === 0) {
+    return;
+  }
+  const children = [];
+  for (const page of files) {
+    children.push(...(page._embedded?.children || []));
+  }
+  const lastPage = files[files.length - 1];
+  return {
+    ...lastPage,
+    _embedded: {
+      ...lastPage._embedded,
+      children
     }
-  );
+  };
+};
+
+const isFilePartial = (f: File) => {
+  return f.partialResult && !f.computationAborted;
+};
+
+const isPartial = (file?: File) => {
+  if (!file) {
+    return false;
+  }
+  if (isFilePartial(file)) {
+    return true;
+  }
+
+  return file._embedded?.children?.some(isFilePartial);
 };
