00d03fa798cb4fb0b0e2c54cc1dcf44bfd7693a5
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2021-04-14 14:07:20 +0200

    Implement full health check for hg

---
+++ scm-plugins/scm-hg-plugin/src/main/java/sonia/scm/repository/spi/HgFullHealthCheckCommand.java
@@ -0,0 +1,54 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020-present Cloudogu GmbH and Contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package sonia.scm.repository.spi;
+
+import com.aragost.javahg.commands.ExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sonia.scm.repository.HealthCheckFailure;
+import sonia.scm.repository.HealthCheckResult;
+
+import java.io.IOException;
+
+public class HgFullHealthCheckCommand extends AbstractCommand implements FullHealthCheckCommand {
+
+  private static final Logger LOG = LoggerFactory.getLogger(HgFullHealthCheckCommand.class);
+
+  public HgFullHealthCheckCommand(HgCommandContext context) {
+    super(context);
+  }
+
+  @Override
+  public HealthCheckResult check() throws IOException {
+    HgVerifyCommand cmd = HgVerifyCommand.on(open());
+    try {
+      cmd.execute();
+      return HealthCheckResult.healthy();
+    } catch (ExecutionException e) {
+      LOG.warn("hg verify failed for repository {}", getRepository(), e);
+      return HealthCheckResult.unhealthy(new HealthCheckFailure("FaSUYbZUR1", "hg verify failed", "The check 'hg verify' failed for the repository."));
+    }
+  }
+}
---
+++ scm-plugins/scm-hg-plugin/src/main/java/sonia/scm/repository/spi/HgVerifyCommand.java
@@ -0,0 +1,50 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020-present Cloudogu GmbH and Contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package sonia.scm.repository.spi;
+
+import com.aragost.javahg.Repository;
+import com.aragost.javahg.internals.AbstractCommand;
+
+public class HgVerifyCommand extends AbstractCommand {
+
+  public static final String COMMAND_NAME = "verify";
+
+  protected HgVerifyCommand(Repository repository) {
+    super(repository, COMMAND_NAME);
+  }
+
+  public static HgVerifyCommand on(Repository repository) {
+    return new HgVerifyCommand(repository);
+  }
+
+  public String execute() {
+    return launchString();
+  }
+
+  @Override
+  public String getCommandName() {
+    return COMMAND_NAME;
+  }
+}
