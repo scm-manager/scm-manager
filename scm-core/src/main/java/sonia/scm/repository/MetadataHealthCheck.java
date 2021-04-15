53f4f9e0bc0d0a86794848fb07ff09abdb0ffc72
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2021-04-15 20:50:33 +0200

    Add quick health check for metadata file

---
+++ scm-core/src/main/java/sonia/scm/repository/MetadataHealthCheck.java
@@ -0,0 +1,56 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020-present Cloudogu GmbH and Contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package sonia.scm.repository;
+
+import sonia.scm.plugin.Extension;
+
+import javax.inject.Inject;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Extension
+public final class MetadataHealthCheck implements HealthCheck {
+
+  public static final HealthCheckFailure METADATA_NOT_WRITABLE =
+    new HealthCheckFailure("6bSUg4dZ41",
+      "metadata file not writable",
+      "The system user has no permissions to modify the metadata file for the repository.");
+  private final RepositoryLocationResolver locationResolver;
+
+  @Inject
+  public MetadataHealthCheck(RepositoryLocationResolver locationResolver) {
+    this.locationResolver = locationResolver;
+  }
+
+  @Override
+  public HealthCheckResult check(Repository repository) {
+    Path repositoryLocation = locationResolver.forClass(Path.class).getLocation(repository.getId());
+    Path metadata = repositoryLocation.resolve("metadata.xml");
+    if (!Files.isWritable(metadata)) {
+      return HealthCheckResult.unhealthy(METADATA_NOT_WRITABLE);
+    }
+    return HealthCheckResult.healthy();
+  }
+}
