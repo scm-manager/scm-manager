84c1e7ed37cfd7fbfe29e6d2c6d1261e08aeb84b
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2021-04-15 11:14:42 +0200

    Add new extension point for repository avatars (#1614)
    Co-authored-by: René Pfeuffer <rene.pfeuffer@cloudogu.com>

--- docs/en/development/plugins/extension-points.md
+++ docs/en/development/plugins/extension-points.md
@@ -33,6 +33,7 @@ The following extension points are provided for the frontend:
 ### profile.route
 ### profile.setting
 ### repo-config.route
+### repo-config.details
 ### repos.branch-details.information
 ### repos.content.metadata
 - Location: At meta data view for file
@@ -46,13 +47,13 @@ The following extension points are provided for the frontend:
 ### repositoryRole.role-details.information
 ### repository.setting
 ### repos.repository-avatar
+### repos.repository-avatar.primary
 - Location: At each repository in repository overview
 - can be used to add avatar for each repository (e.g., to mark repository type)
 
 ### repos.repository-details.information
 - Location: At bottom of a single repository view
 - can be used to show detailed information about the repository (how to clone, e.g.)
-
 ### repos.sources.view
 ### roles.route
 ### user.route
--- scm-ui/ui-components/src/forms/FileUpload.tsx
+++ scm-ui/ui-components/src/forms/FileUpload.tsx
@@ -27,10 +27,12 @@ import { useTranslation } from "react-i18next";
 import { File } from "@scm-manager/ui-types";
 
 type Props = {
-  handleFile: (file: File) => void;
+  handleFile: (file: File, event?: ChangeEvent<HTMLInputElement>) => void;
+  filenamePlaceholder?: string;
+  disabled?: boolean;
 };
 
-const FileUpload: FC<Props> = ({ handleFile }) => {
+const FileUpload: FC<Props> = ({ handleFile, filenamePlaceholder = "", disabled = false }) => {
   const [t] = useTranslation("commons");
   const [file, setFile] = useState<File | null>(null);
 
@@ -41,21 +43,26 @@ const FileUpload: FC<Props> = ({ handleFile }) => {
           className="file-input"
           type="file"
           name="resume"
+          disabled={disabled}
           onChange={(event: ChangeEvent<HTMLInputElement>) => {
             const uploadedFile = event?.target?.files![0];
             // @ts-ignore the uploaded file doesn't match our types
             setFile(uploadedFile);
             // @ts-ignore the uploaded file doesn't match our types
-            handleFile(uploadedFile);
+            handleFile(uploadedFile, event);
           }}
         />
         <span className="file-cta">
           <span className="file-icon">
             <i className="fas fa-arrow-circle-up" />
           </span>
-          <span className="file-label">{t("fileUpload.label")}</span>
+          <span className="file-label has-text-weight-bold">{t("fileUpload.label")}</span>
         </span>
-        <span className="file-name">{file?.name || ""}</span>
+        {file?.name ? (
+          <span className="file-name">{file?.name}</span>
+        ) : (
+          <span className="file-name has-text-weight-light has-text-grey-light	">{filenamePlaceholder}</span>
+        )}
       </label>
     </div>
   );
