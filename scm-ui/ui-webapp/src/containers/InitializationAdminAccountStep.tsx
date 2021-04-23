72417fed09435aba19f028734d896eb5ca7a6093
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2021-04-23 11:47:37 +0200

    Disable submit button when form is invalid

--- scm-ui/ui-webapp/src/containers/InitializationAdminAccountStep.tsx
+++ scm-ui/ui-webapp/src/containers/InitializationAdminAccountStep.tsx
@@ -131,80 +131,91 @@ const InitializationAdminAccountStep: FC<Props> = ({ data }) => {
     });
   };
 
-  let component;
+  let errorComponent;
   if (error) {
-    component = <ErrorNotification error={error} />;
-  } else {
-    component = (
-      <div className="column is-8 box  has-background-white-ter">
-        <h3 className="title">{t("title")}</h3>
-        <h4 className="subtitle">{t("adminStep.title")}</h4>
-        <p>{t("adminStep.description")}</p>
-        <form onSubmit={handleSubmit}>
-          <div className={"columns"}>
-            <div className="column is-full-width">
-              <InputField placeholder={t("adminStep.startupToken")} autofocus={true} onChange={setStartupKey} />
-            </div>
-          </div>
-          <div className={"columns"}>
-            <div className="column is-half">
-              <InputField
-                testId="username-input"
-                label={t("adminStep.username")}
-                onChange={validateAndSetUserName}
-                validationError={userNameValidationError}
-                value={userName}
-              />
-            </div>
-            <div className="column is-half">
-              <InputField
-                testId="displayname-input"
-                label={t("adminStep.displayname")}
-                onChange={validateAndSetDisplayName}
-                value={displayName}
-                validationError={displayNameValidationError}
-              />
-            </div>
-          </div>
-          <div className={"columns"}>
-            <div className="column is-full-width">
-              <InputField
-                label={t("adminStep.email")}
-                onChange={validateAndSetEmail}
-                value={email}
-                validationError={mailValidationError}
-              />
-            </div>
-          </div>
-          <div className={"columns"}>
-            <div className="column is-half">
-              <InputField
-                testId="password-input"
-                label={t("adminStep.password")}
-                type="password"
-                onChange={validateAndSetPassword}
-                validationError={passwordValidationError}
-              />
-            </div>
-            <div className="column is-half">
-              <InputField
-                testId="password-confirmation-input"
-                label={t("adminStep.password-confirmation")}
-                type="password"
-                onChange={validateAndSetPasswordConfirmation}
-                validationError={passwordConfirmationValidationError}
-              />
-            </div>
-          </div>
-          <div className={"columns"}>
-            <div className="column is-full-width">
-              <SubmitButton label={t("adminStep.submit")} fullWidth={true} loading={isLoading} />
-            </div>
-          </div>
-        </form>
-      </div>
-    );
+    errorComponent = <ErrorNotification error={error} />;
   }
+
+  const formValid =
+    !!(startupKey && userName && displayName && email && password && passwordConfirmation) &&
+    !(
+      userNameValidationError ||
+      displayNameValidationError ||
+      mailValidationError ||
+      passwordValidationError ||
+      passwordConfirmationValidationError
+    );
+  const component = (
+    <div className="column is-8 box  has-background-white-ter">
+      <h3 className="title">{t("title")}</h3>
+      <h4 className="subtitle">{t("adminStep.title")}</h4>
+      {errorComponent}
+      <p>{t("adminStep.description")}</p>
+      <form onSubmit={handleSubmit}>
+        <div className={"columns"}>
+          <div className="column is-full-width">
+            <InputField placeholder={t("adminStep.startupToken")} autofocus={true} onChange={setStartupKey} />
+          </div>
+        </div>
+        <div className={"columns"}>
+          <div className="column is-half">
+            <InputField
+              testId="username-input"
+              label={t("adminStep.username")}
+              onChange={validateAndSetUserName}
+              validationError={userNameValidationError}
+              value={userName}
+            />
+          </div>
+          <div className="column is-half">
+            <InputField
+              testId="displayname-input"
+              label={t("adminStep.displayname")}
+              onChange={validateAndSetDisplayName}
+              value={displayName}
+              validationError={displayNameValidationError}
+            />
+          </div>
+        </div>
+        <div className={"columns"}>
+          <div className="column is-full-width">
+            <InputField
+              label={t("adminStep.email")}
+              onChange={validateAndSetEmail}
+              value={email}
+              validationError={mailValidationError}
+            />
+          </div>
+        </div>
+        <div className={"columns"}>
+          <div className="column is-half">
+            <InputField
+              testId="password-input"
+              label={t("adminStep.password")}
+              type="password"
+              onChange={validateAndSetPassword}
+              validationError={passwordValidationError}
+            />
+          </div>
+          <div className="column is-half">
+            <InputField
+              testId="password-confirmation-input"
+              label={t("adminStep.password-confirmation")}
+              type="password"
+              onChange={validateAndSetPasswordConfirmation}
+              validationError={passwordConfirmationValidationError}
+            />
+          </div>
+        </div>
+        <div className={"columns"}>
+          <div className="column is-full-width">
+            <SubmitButton label={t("adminStep.submit")} fullWidth={true} loading={isLoading} disabled={!formValid} />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+
   return (
     <HeroSection className="hero">
       <div className="hero-body">
