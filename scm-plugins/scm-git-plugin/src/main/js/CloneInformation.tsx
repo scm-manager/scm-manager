af8980de19b580f80c8ec5f72428071d6b45dbfb
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2021-04-29 18:13:32 +0200

    Improve repository information page (#1636)
    Only show relevant information for repository on repository information page. The initialization code example is only shown if the repository is still empty.

--- scm-plugins/scm-git-plugin/src/main/js/CloneInformation.tsx
+++ scm-plugins/scm-git-plugin/src/main/js/CloneInformation.tsx
@@ -21,57 +21,93 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-import React from "react";
-import { WithTranslation, withTranslation } from "react-i18next";
-import { Repository } from "@scm-manager/ui-types";
+import React, { FC, useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { Link, Repository } from "@scm-manager/ui-types";
+import { apiClient } from "@scm-manager/ui-components";
 
-type Props = WithTranslation & {
+type Props = {
   url: string;
   repository: Repository;
 };
 
-class CloneInformation extends React.Component<Props> {
-  render() {
-    const { url, repository, t } = this.props;
+const CloneInformation: FC<Props> = ({ url, repository }) => {
+  const [t] = useTranslation("plugins");
+  const [defaultBranch, setDefaultBranch] = useState<string>("main");
+  const [emptyRepository, setEmptyRepository] = useState<boolean>();
 
-    return (
-      <div>
-        <h4>{t("scm-git-plugin.information.clone")}</h4>
-        <pre>
-          <code>git clone {url}</code>
-        </pre>
-        <h4>{t("scm-git-plugin.information.create")}</h4>
-        <pre>
-          <code>
-            git init {repository.name}
-            <br />
-            cd {repository.name}
-            <br />
-            echo "# {repository.name}
-            " &gt; README.md
-            <br />
-            git add README.md
-            <br />
-            git commit -m "Add readme"
-            <br />
-            git remote add origin {url}
-            <br />
-            git push -u origin master
-            <br />
-          </code>
-        </pre>
-        <h4>{t("scm-git-plugin.information.replace")}</h4>
-        <pre>
-          <code>
-            git remote add origin {url}
-            <br />
-            git push -u origin master
-            <br />
-          </code>
-        </pre>
-      </div>
-    );
-  }
-}
+  useEffect(() => {
+    if (repository) {
+      apiClient
+        .get((repository._links.changesets as Link).href + "?limit=1")
+        .then(r => r.json())
+        .then(result => {
+          const empty = result._embedded.changesets.length === 0;
+          setEmptyRepository(empty);
+        });
+    }
+  }, [repository]);
 
-export default withTranslation("plugins")(CloneInformation);
+  useEffect(() => {
+    if (repository) {
+      apiClient
+        .get((repository._links.defaultBranch as Link).href)
+        .then(r => r.json())
+        .then(r => r.defaultBranch && setDefaultBranch(r.defaultBranch));
+    }
+  }, [repository]);
+
+  return (
+    <div>
+      <h4>{t("scm-git-plugin.information.clone")}</h4>
+      <pre>
+        <code>
+          git clone {url}
+          <br />
+          cd {repository.name}
+          {emptyRepository && (
+            <>
+              <br />
+              git checkout -b {defaultBranch}
+            </>
+          )}
+        </code>
+      </pre>
+      {emptyRepository && (
+        <>
+          <h4>{t("scm-git-plugin.information.create")}</h4>
+          <pre>
+            <code>
+              git init {repository.name}
+              <br />
+              cd {repository.name}
+              <br />
+              git checkout -b {defaultBranch}
+              <br />
+              echo "# {repository.name}
+              " &gt; README.md
+              <br />
+              git add README.md
+              <br />
+              git commit -m "Add readme"
+              <br />
+              git remote add origin {url}
+              <br />
+              git push -u origin {defaultBranch}
+              <br />
+            </code>
+          </pre>
+        </>
+      )}
+      <h4>{t("scm-git-plugin.information.replace")}</h4>
+      <pre>
+        <code>
+          git remote add origin {url}
+          <br />
+        </code>
+      </pre>
+    </div>
+  );
+};
+
+export default CloneInformation;
---
+++ scm-plugins/scm-git-plugin/src/test/java/sonia/scm/api/v2/resources/RepositoryLinkEnricherTest.java
@@ -0,0 +1,90 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020-present Cloudogu GmbH and Contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package sonia.scm.api.v2.resources;
+
+import com.google.inject.util.Providers;
+import org.github.sdorra.jse.ShiroExtension;
+import org.github.sdorra.jse.SubjectAware;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import sonia.scm.repository.Repository;
+import sonia.scm.repository.RepositoryTestData;
+
+import javax.inject.Provider;
+import java.net.URI;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(ShiroExtension.class)
+@ExtendWith(MockitoExtension.class)
+@SubjectAware(
+  value = "trillian"
+)
+class RepositoryLinkEnricherTest {
+
+  private static final Repository REPOSITORY = RepositoryTestData.create42Puzzle();
+
+  private RepositoryLinkEnricher repositoryLinkEnricher;
+
+  @Mock
+  private HalAppender appender;
+
+  @BeforeEach
+  void initEnricher() {
+    ScmPathInfoStore scmPathInfoStore = new ScmPathInfoStore();
+    scmPathInfoStore.set(() -> URI.create("/api/"));
+    Provider<ScmPathInfoStore> scmPathInfoStoreProvider = Providers.of(scmPathInfoStore);
+    repositoryLinkEnricher = new RepositoryLinkEnricher(scmPathInfoStoreProvider);
+    REPOSITORY.setId("id-1");
+  }
+
+  @Test
+  void shouldNotAppendLinkIfNotPermitted() {
+    HalEnricherContext context = HalEnricherContext.of(REPOSITORY);
+
+    repositoryLinkEnricher.enrich(context, appender);
+
+    verify(appender, never()).appendLink(eq("defaultBranch"), any(String.class));
+  }
+
+  @Test
+  @SubjectAware(
+    permissions = "repository:read:id-1"
+  )
+  void shouldAppendDefaultBranchLink() {
+    HalEnricherContext context = HalEnricherContext.of(REPOSITORY);
+
+    repositoryLinkEnricher.enrich(context, appender);
+
+    verify(appender).appendLink("defaultBranch", "/api/v2/config/git/hitchhiker/42Puzzle/default-branch");
+  }
+
+}
