ea7be13b173def89ac73e911cdea6c4aee335c7a
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2021-05-14 09:15:17 +0200

    Test credentials

--- scm-plugins/scm-git-plugin/src/test/java/sonia/scm/repository/spi/GitMirrorCommandTest.java
+++ scm-plugins/scm-git-plugin/src/test/java/sonia/scm/repository/spi/GitMirrorCommandTest.java
@@ -26,36 +26,49 @@ package sonia.scm.repository.spi;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.junit.http.AppServer;
+import org.eclipse.jgit.junit.http.SimpleHttpServer;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.revwalk.RevObject;
 import org.eclipse.jgit.revwalk.RevWalk;
+import org.junit.Before;
 import org.junit.Test;
 import sonia.scm.api.v2.resources.GitRepositoryConfigStoreProvider;
 import sonia.scm.repository.GitConfig;
 import sonia.scm.repository.api.MirrorCommandResult;
+import sonia.scm.repository.api.SimpleUsernamePasswordCredential;
 import sonia.scm.store.InMemoryConfigurationStoreFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.function.Consumer;
 
+import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 public class GitMirrorCommandTest extends AbstractGitCommandTestBase {
 
-  PostReceiveRepositoryHookEventFactory postReceiveRepositoryHookEventFactory = mock(PostReceiveRepositoryHookEventFactory.class);
+  private final PostReceiveRepositoryHookEventFactory postReceiveRepositoryHookEventFactory = mock(PostReceiveRepositoryHookEventFactory.class);
+
+  private File clone;
+  private GitContext emptyContext;
+
+  @Before
+  public void bendContextToNewRepository() throws IOException, GitAPIException {
+    clone = tempFolder.newFolder();
+    Git.init().setBare(true).setDirectory(clone).call();
+
+    emptyContext = createMirrorContext(clone);
+  }
 
   @Test
   public void shouldCreateInitialMirror() throws IOException, GitAPIException {
-    File clone = tempFolder.newFolder();
-    Git.init().setBare(true).setDirectory(clone).call();
-
-    GitContext context = createMirrorContext(clone);
-
-    MirrorCommandResult result = callMirrorCommand(context);
+    MirrorCommandResult result = callMirrorCommand(emptyContext);
 
     assertThat(result.isSuccess()).isTrue();
     assertThat(result.getLog()).contains("Branches:")
@@ -72,23 +85,9 @@ public class GitMirrorCommandTest extends AbstractGitCommandTestBase {
     verify(postReceiveRepositoryHookEventFactory).fireForFetch(any(), any());
   }
 
-  private MirrorCommandResult callMirrorCommand(GitContext context) {
-    GitMirrorCommand command = new GitMirrorCommand(context, postReceiveRepositoryHookEventFactory);
-    MirrorCommandRequest request = new MirrorCommandRequest();
-    request.setSourceUrl(repositoryDirectory.getAbsolutePath());
-    return command.mirror(request);
-  }
-
-  private GitContext createMirrorContext(File clone) {
-    return new GitContext(clone, repository, new GitRepositoryConfigStoreProvider(InMemoryConfigurationStoreFactory.create()), new GitConfig());
-  }
-
   @Test
   public void shouldUpdateMirror() throws IOException, GitAPIException {
-    File clone = tempFolder.newFolder();
-    Git.init().setBare(true).setDirectory(clone).call();
-    GitContext context = createMirrorContext(clone);
-    callMirrorCommand(context);
+    callMirrorCommand(emptyContext);
 
     try (Git existingClone = Git.open(repositoryDirectory)) {
       existingClone.branchCreate().setName("addedBranch").call();
@@ -98,7 +97,7 @@ public class GitMirrorCommandTest extends AbstractGitCommandTestBase {
       existingClone.tag().setName("addedTag").setAnnotated(false).setObjectId(revObject).call();
     }
 
-    GitMirrorCommand command = new GitMirrorCommand(context, postReceiveRepositoryHookEventFactory);
+    GitMirrorCommand command = new GitMirrorCommand(emptyContext, postReceiveRepositoryHookEventFactory);
     MirrorCommandRequest request = new MirrorCommandRequest();
     request.setSourceUrl(repositoryDirectory.getAbsolutePath());
     MirrorCommandResult result = command.update(request);
@@ -115,5 +114,68 @@ public class GitMirrorCommandTest extends AbstractGitCommandTestBase {
 
     // event should be thrown two times, once for the initial clone, and once for the update
     verify(postReceiveRepositoryHookEventFactory, times(2)).fireForFetch(any(), any());
+  }
+
+  @Test
+  public void shouldUseCredentials() throws Exception {
+    SimpleHttpServer simpleHttpServer = new SimpleHttpServer(Git.open(repositoryDirectory).getRepository());
+    simpleHttpServer.start();
+
+    try {
+      MirrorCommandResult result =
+        callMirrorCommand(
+          emptyContext,
+          simpleHttpServer.getUri().toASCIIString(),
+          createCredential(AppServer.username, AppServer.password));
+
+      assertThat(result.isSuccess()).isTrue();
+      assertThat(result.getLog()).contains("Branches:")
+        .contains("- 000000000..fcd0ef183 master")
+        .contains("- 000000000..3f76a12f0 test-branch")
+        .contains("Tags:")
+        .contains("- 000000000..86a6645ec test-tag");
+    } finally {
+      simpleHttpServer.stop();
+    }
+  }
+
+  @Test
+  public void shouldFailWithIncorrectCredentials() throws Exception {
+    SimpleHttpServer simpleHttpServer = new SimpleHttpServer(Git.open(repositoryDirectory).getRepository());
+    simpleHttpServer.start();
+
+    try {
+      MirrorCommandResult result =
+        callMirrorCommand(
+          emptyContext,
+          simpleHttpServer.getUri().toASCIIString(),
+          createCredential("wrong", "credentials"));
+
+      assertThat(result.isSuccess()).isFalse();
+
+      verify(postReceiveRepositoryHookEventFactory, never()).fireForFetch(any(), any());
+    } finally {
+      simpleHttpServer.stop();
+    }
+  }
+
+  private MirrorCommandResult callMirrorCommand(GitContext context) {
+    return callMirrorCommand(context, repositoryDirectory.getAbsolutePath(), c -> {});
+  }
+
+  private MirrorCommandResult callMirrorCommand(GitContext context, String source, Consumer<MirrorCommandRequest> requestConsumer) {
+    GitMirrorCommand command = new GitMirrorCommand(context, postReceiveRepositoryHookEventFactory);
+    MirrorCommandRequest request = new MirrorCommandRequest();
+    request.setSourceUrl(source);
+    requestConsumer.accept(request);
+    return command.mirror(request);
+  }
+
+  private Consumer<MirrorCommandRequest> createCredential(String wrong, String credentials) {
+    return request -> request.setCredentials(singletonList(new SimpleUsernamePasswordCredential(wrong, credentials.toCharArray())));
+  }
+
+  private GitContext createMirrorContext(File clone) {
+    return new GitContext(clone, repository, new GitRepositoryConfigStoreProvider(InMemoryConfigurationStoreFactory.create()), new GitConfig());
   }
 }
