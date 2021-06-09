fef274225cb36de4f12bef946cf7f6fc4493d4bf
Florian Scholdei <florian.scholdei@cloudogu.com>
2021-06-09 10:12:04 +0200

    Harmonize FileInput components with styleguide

---
+++ scm-ui/ui-components/src/forms/FileInput.stories.tsx
@@ -0,0 +1,69 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020-present Cloudogu GmbH and Contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+import React, { FC, useState } from "react";
+import { MemoryRouter } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { storiesOf } from "@storybook/react";
+import { Level, SubmitButton } from "../index";
+import FileInput from "./FileInput";
+import styled from "styled-components";
+
+const Decorator = styled.div`
+  padding: 2rem;
+  max-width: 40rem;
+`;
+
+type Settings = {
+  uploadFile: string;
+};
+
+const ReactHookForm: FC = () => {
+  const { register, handleSubmit } = useForm<Settings>();
+  const [stored, setStored] = useState<Settings>();
+
+  const onSubmit = (settings: Settings) => {
+    setStored(settings);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FileInput label="Upload File" helpText="Select your most loved file" {...register("uploadFile")} />
+        <Level right={<SubmitButton>Submit</SubmitButton>} />
+      </form>
+      {stored ? (
+        <div className="mt-5">
+          <pre>
+            <code>{JSON.stringify(stored, null, 2)}</code>
+          </pre>
+        </div>
+      ) : null}
+    </>
+  );
+};
+
+storiesOf("Forms|FileInput", module)
+  .addDecorator((storyFn) => <Decorator>{storyFn()}</Decorator>)
+  .addDecorator((storyFn) => <MemoryRouter>{storyFn()}</MemoryRouter>)
+  .add("Default", () => <ReactHookForm />);
