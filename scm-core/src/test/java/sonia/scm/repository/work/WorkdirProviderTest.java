40cfbe728f81eec52d07de967c237e6d0f2a9d3b
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2021-07-27 10:48:26 +0200

    Delete old work dirs on startup and shutdown

--- scm-core/src/test/java/sonia/scm/repository/work/WorkdirProviderTest.java
+++ scm-core/src/test/java/sonia/scm/repository/work/WorkdirProviderTest.java
@@ -25,6 +25,7 @@
 package sonia.scm.repository.work;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.io.TempDir;
@@ -34,10 +35,15 @@ import sonia.scm.repository.RepositoryLocationResolver;
 import sonia.scm.repository.RepositoryLocationResolver.RepositoryLocationResolverInstance;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.function.BiConsumer;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
@@ -52,7 +58,7 @@ class WorkdirProviderTest {
   private RepositoryLocationResolverInstance<Path> repositoryLocationResolverInstance;
 
   @BeforeEach
-  void initResolver(@TempDir Path temp) {
+  void initResolver() {
     lenient().when(repositoryLocationResolver.forClass(Path.class)).thenReturn(repositoryLocationResolverInstance);
   }
 
@@ -62,8 +68,9 @@ class WorkdirProviderTest {
 
     File newWorkdir = provider.createNewWorkdir();
 
-    assertThat(newWorkdir).exists();
-    assertThat(newWorkdir).hasParent(temp.toFile());
+    assertThat(newWorkdir)
+      .exists()
+      .hasParent(temp.toFile());
     verify(repositoryLocationResolverInstance, never()).getLocation(anyString());
   }
 
@@ -86,8 +93,74 @@ class WorkdirProviderTest {
 
     File newWorkdir = provider.createNewWorkdir("42");
 
-    assertThat(newWorkdir).exists();
-    assertThat(newWorkdir).hasParent(temp.toFile());
+    assertThat(newWorkdir)
+      .exists()
+      .hasParent(temp.toFile());
     verify(repositoryLocationResolverInstance, never()).getLocation(anyString());
+  }
+
+  @Nested
+  class WithExistingGlobalWorkDir {
+
+    private Path globalRootDir;
+    private WorkdirProvider provider;
+
+    @BeforeEach
+    void createExistingWorkDir(@TempDir Path temp) throws IOException {
+      globalRootDir = temp.resolve("global");
+      Files.createDirectories(globalRootDir.resolve("global-temp"));
+
+      provider = new WorkdirProvider(globalRootDir.toFile(), repositoryLocationResolver, true);
+    }
+
+    @Test
+    void shouldDeleteOldGlobalWorkDirsOnStartup() {
+      provider.contextInitialized(null);
+
+      assertThat(globalRootDir).isEmptyDirectory();
+    }
+
+    @Test
+    void shouldDeleteOldGlobalWorkDirsOnShutdown() {
+      provider.contextDestroyed(null);
+
+      assertThat(globalRootDir).isEmptyDirectory();
+    }
+  }
+
+  @Nested
+  class WithExistingRepositoryWorkDir {
+
+    private Path repositoryRootDir;
+    private WorkdirProvider provider;
+
+    @BeforeEach
+    void createExistingWorkDir(@TempDir Path temp) throws IOException {
+      repositoryRootDir = temp.resolve("42");
+      Files.createDirectories(repositoryRootDir.resolve("work").resolve("repo-temp"));
+
+      doAnswer(
+        invocationOnMock -> {
+          invocationOnMock.getArgument(0, BiConsumer.class)
+            .accept("42", repositoryRootDir);
+          return null;
+        }
+      ).when(repositoryLocationResolverInstance).forAllLocations(any());
+      provider = new WorkdirProvider(temp.resolve("global").toFile(), repositoryLocationResolver, true);
+    }
+
+    @Test
+    void shouldDeleteOldRepositoryRelatedWorkDirsOnStartup() {
+      provider.contextInitialized(null);
+
+      assertThat(repositoryRootDir.resolve("work")).isEmptyDirectory();
+    }
+
+    @Test
+    void shouldDeleteOldRepositoryRelatedWorkDirsOnShutdown() {
+      provider.contextInitialized(null);
+
+      assertThat(repositoryRootDir.resolve("work")).isEmptyDirectory();
+    }
   }
 }
