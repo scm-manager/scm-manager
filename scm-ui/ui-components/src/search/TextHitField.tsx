1a6d22aa7f91d39d266f1298a8994c0b46895903
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2021-08-04 09:48:46 +0200

    Add option to cut value of long fields

--- scm-ui/ui-components/src/search/TextHitField.tsx
+++ scm-ui/ui-components/src/search/TextHitField.tsx
@@ -30,6 +30,7 @@ import { isHighlightedHitField } from "./fields";
 type Props = {
   hit: Hit;
   field: string;
+  cutValueAt?: number;
 };
 
 type HighlightedTextFieldProps = {
@@ -48,14 +49,18 @@ const HighlightedTextField: FC<HighlightedTextFieldProps> = ({ field }) => (
   </>
 );
 
-const TextHitField: FC<Props> = ({ hit, field: fieldName }) => {
+const TextHitField: FC<Props> = ({ hit, field: fieldName, cutValueAt = 0 }) => {
   const field = hit.fields[fieldName];
   if (!field) {
     return null;
   } else if (isHighlightedHitField(field)) {
     return <HighlightedTextField field={field} />;
   } else {
-    return <>{field.value}</>;
+    let value = field.value;
+    if (typeof value === "string" && cutValueAt > 0 && value.length > cutValueAt) {
+      value = value.substring(0, cutValueAt) + "...";
+    }
+    return <>{value}</>;
   }
 };
 
