afdadb083179d5f1e81968135fd2de5fee07cc7f
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2021-08-05 15:34:53 +0200

    Implement base commit feature for modifications command for svn

---
+++ scm-plugins/scm-svn-plugin/src/main/java/sonia/scm/repository/ConsolidatingModificationCollector.java
@@ -0,0 +1,119 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020-present Cloudogu GmbH and Contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package sonia.scm.repository;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+import static java.util.Collections.emptySet;
+import static java.util.Collections.unmodifiableCollection;
+import static java.util.Collections.unmodifiableSet;
+
+class ConsolidatingModificationCollector implements Collector<Modification, ConsolidatedModifications, Collection<Modification>> {
+
+  static ConsolidatingModificationCollector consolidate() {
+    return new ConsolidatingModificationCollector();
+  }
+
+  @Override
+  public Supplier<ConsolidatedModifications> supplier() {
+    return ConsolidatedModifications::new;
+  }
+
+  @Override
+  public BiConsumer<ConsolidatedModifications, Modification> accumulator() {
+    return (existingModifications, currentModification) -> {
+      if (currentModification instanceof Copied) {
+        existingModifications.added(new Added(((Copied) currentModification).getTargetPath()));
+      } else if (currentModification instanceof Removed) {
+        existingModifications.removed((Removed) currentModification);
+      } else if (currentModification instanceof Renamed) {
+        Renamed renameModification = (Renamed) currentModification;
+        existingModifications.removed(new Removed(renameModification.getOldPath()));
+        existingModifications.added(new Added(renameModification.getNewPath()));
+      } else if (currentModification instanceof Added) {
+        existingModifications.added((Added) currentModification);
+      } else if (currentModification instanceof Modified) {
+        existingModifications.modified((Modified) currentModification);
+      } else {
+        throw new IllegalStateException("cannot handle modification of unknown type " + currentModification.getClass());
+      }
+    };
+  }
+
+  @Override
+  public BinaryOperator<ConsolidatedModifications> combiner() {
+    return null; // Combiner not needed because we do not support Collector.Characteristics#CONCURRENT
+  }
+
+  @Override
+  public Function<ConsolidatedModifications, Collection<Modification>> finisher() {
+    return ConsolidatedModifications::getModifications;
+  }
+
+  @Override
+  public Set<Characteristics> characteristics() {
+    return emptySet();
+  }
+}
+
+class ConsolidatedModifications {
+  private final Map<String, Modification> modifications = new HashMap<>();
+
+  Set<String> getPaths() {
+    return unmodifiableSet(modifications.keySet());
+  }
+
+  Collection<Modification> getModifications() {
+    return unmodifiableCollection(modifications.values());
+  }
+
+  void added(Added added) {
+    modifications.put(added.getPath(), added);
+  }
+
+  void modified(Modified modified) {
+    Modification earlierModification = modifications.get(modified.getPath());
+    if (!(earlierModification instanceof Added)) { // added should still be added
+      modified.getEffectedPaths().forEach(path -> modifications.put(path, modified));
+    }
+  }
+
+  void removed(Removed removed) {
+    Modification earlierModification = modifications.get(removed.getPath());
+    if (earlierModification instanceof Added) {
+      modifications.remove(removed.getPath());
+    } else {
+      modifications.put(removed.getPath(), removed);
+    }
+  }
+}
---
+++ scm-plugins/scm-svn-plugin/src/test/java/sonia/scm/repository/ConsolidatingModificationCollectorTest.java
@@ -0,0 +1,120 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020-present Cloudogu GmbH and Contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package sonia.scm.repository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Collection;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class ConsolidatingModificationCollectorTest {
+
+  @Test
+  void shouldKeepIndependentChanges() {
+    Collection<Modification> consolidated =
+      Stream.of(
+        new Added("added"),
+        new Removed("removed"),
+        new Modified("modified")
+      ).collect(new ConsolidatingModificationCollector());
+
+    assertThat(consolidated)
+      .extracting("class")
+      .containsExactlyInAnyOrder(Added.class, Removed.class, Modified.class);
+  }
+
+  @Test
+  void shouldNotListAddedFileIfRemovedLaterOn() {
+    Collection<Modification> consolidated =
+      Stream.of(
+        new Added("file"),
+        new Removed("file")
+      ).collect(new ConsolidatingModificationCollector());
+
+    assertThat(consolidated)
+      .isEmpty();
+  }
+
+  @Test
+  void shouldReplaceModificationWithRemove() {
+    Collection<Modification> consolidated =
+      Stream.of(
+        new Modified("file"),
+        new Removed("file")
+      ).collect(new ConsolidatingModificationCollector());
+
+    assertThat(consolidated)
+      .extracting("class")
+      .containsExactly(Removed.class);
+  }
+
+  @Test
+  void shouldReplaceCopyWithAdd() {
+    Collection<Modification> consolidated =
+      Stream.of(
+        new Copied("source", "target")
+      ).collect(new ConsolidatingModificationCollector());
+
+    assertThat(consolidated)
+      .extracting("class")
+      .containsExactly(Added.class);
+    assertThat(consolidated)
+      .extracting("path")
+      .containsExactly("target");
+  }
+
+  @Test
+  void shouldReplaceRenameWithAddAndRemove() {
+    Collection<Modification> consolidated =
+      Stream.of(
+        new Renamed("source", "target")
+      ).collect(new ConsolidatingModificationCollector());
+
+    assertThat(consolidated)
+      .extracting("class")
+      .containsExactlyInAnyOrder(Added.class, Removed.class);
+    assertThat(consolidated)
+      .extracting("path")
+      .containsExactlyInAnyOrder("source", "target");
+  }
+
+  @Test
+  void shouldNotReplaceAddWithModify() {
+    Collection<Modification> consolidated =
+      Stream.of(
+        new Added("file"),
+        new Modified("file")
+      ).collect(new ConsolidatingModificationCollector());
+
+    assertThat(consolidated)
+      .extracting("class")
+      .containsExactlyInAnyOrder(Added.class);
+  }
+}
