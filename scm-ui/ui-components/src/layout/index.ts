1dc1349b2a70745c69a7ffc722752613cfd9e2ae
Konstantin Schaper <konstantin.schaper@cloudogu.com>
2021-10-15 10:22:45 +0200

    remove broken export

--- scm-ui/ui-components/src/layout/index.ts
+++ scm-ui/ui-components/src/layout/index.ts
@@ -34,4 +34,3 @@ export { default as Title } from "./Title";
 export { default as CustomQueryFlexWrappedColumns } from "./CustomQueryFlexWrappedColumns";
 export { default as PrimaryContentColumn } from "./PrimaryContentColumn";
 export { default as SecondaryNavigationColumn } from "./SecondaryNavigationColumn";
-export { default as GroupEntries } from "./GroupEntries";
