30839102707f76f37e5b9d913f0b4a8331439dcc
Sebastian Sdorra <s.sdorra@gmail.com>
2021-11-08 08:29:13 +0100

    Update node to version 16.13.0

--- build-plugins/src/main/groovy/com/cloudogu/scm/RunPlugin.groovy
+++ build-plugins/src/main/groovy/com/cloudogu/scm/RunPlugin.groovy
@@ -37,7 +37,7 @@ class RunPlugin implements Plugin<Project> {
     project.plugins.apply("com.github.node-gradle.node")
     def nodeExt = NodeExtension.get(project)
     nodeExt.setDownload(true)
-    nodeExt.setVersion('14.15.1')
+    nodeExt.setVersion('16.13.0')
     nodeExt.setYarnVersion('1.22.15')
     nodeExt.setNodeModulesDir( project.rootProject.projectDir )
 
