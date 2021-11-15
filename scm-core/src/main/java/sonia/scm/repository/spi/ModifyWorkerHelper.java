bdd35ceb31edf6bd4278836abd5927e2510f745b
Konstantin Schaper <konstantin.schaper@cloudogu.com>
2021-11-15 15:56:57 +0100

    remove unused import

--- scm-core/src/main/java/sonia/scm/repository/spi/ModifyWorkerHelper.java
+++ scm-core/src/main/java/sonia/scm/repository/spi/ModifyWorkerHelper.java
@@ -39,7 +39,6 @@ import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
