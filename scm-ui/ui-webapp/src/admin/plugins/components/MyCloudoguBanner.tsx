3edd7d23887f7060b4d7857d241e62570a31040b
Konstantin Schaper <konstantin.schaper@cloudogu.com>
2021-11-30 15:44:08 +0100

    fix styling

--- scm-ui/ui-webapp/src/admin/plugins/components/MyCloudoguBanner.tsx
+++ scm-ui/ui-webapp/src/admin/plugins/components/MyCloudoguBanner.tsx
@@ -1,11 +1,11 @@
 import { Button, Image } from "@scm-manager/ui-components";
 import * as React from "react";
 import styled from "styled-components";
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import { usePluginCenterLogin } from "@scm-manager/ui-api";
 
 const MyCloudoguBannerWrapper = styled.div`
-  border: 1px solid #123;
+  border: 1px solid;
 `;
 
 const TextImage = styled(Image)`
@@ -17,11 +17,21 @@ const MyCloudoguBanner = () => {
   const login = usePluginCenterLogin();
   const [t] = useTranslation("admin");
   return login ? (
-    <MyCloudoguBannerWrapper className="has-rounded-border is-flex is-flex-direction-column is-align-items-center p-5 mb-4">
-      <Button className="mb-5" reducedMobile={true} label={t("plugins.myCloudogu.login.button.label")} link={login}>
-        <TextImage alt="myCloudogu" src="/images/myCloudogu.svg" className="ml-1" />
+    <MyCloudoguBannerWrapper className="has-rounded-border is-flex is-flex-direction-column is-align-items-center p-5 mb-4 has-border-success">
+      <Button className="mb-5 has-text-weight-normal has-border-info" reducedMobile={true} link={login}>
+        <Trans
+          t={t}
+          i18nKey="plugins.myCloudogu.login.button.label"
+          components={[<TextImage alt="myCloudogu" src="/images/myCloudogu.svg" className="mx-1" />]}
+        />
       </Button>
-      <p className="is-align-self-flex-start is-size-7">{t("plugins.myCloudogu.login.description")}</p>
+      <p className="is-align-self-flex-start is-size-7">
+        <Trans
+          t={t}
+          i18nKey="plugins.myCloudogu.login.description"
+          components={[<a href="https://my.cloudogu.com/">myCloudogu</a>]}
+        />
+      </p>
     </MyCloudoguBannerWrapper>
   ) : null;
 };
