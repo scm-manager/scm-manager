86a5d390a8a3aea47a4ad0dc5e9ce9e3d939d353
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2021-12-01 15:57:36 +0100

    Fix footer links

--- scm-ui/ui-webapp/src/containers/Profile.tsx
+++ scm-ui/ui-webapp/src/containers/Profile.tsx
@@ -22,7 +22,7 @@
  * SOFTWARE.
  */
 import React, { FC } from "react";
-import { Redirect, Route, useRouteMatch } from "react-router-dom";
+import { Route, useRouteMatch } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import {
   CustomQueryFlexWrappedColumns,
@@ -34,7 +34,7 @@ import {
   SecondaryNavigationColumn,
   StateMenuContextProvider,
   SubNavigation,
-  urls,
+  urls
 } from "@scm-manager/ui-components";
 import ChangeUserPassword from "./ChangeUserPassword";
 import ProfileInfo from "./ProfileInfo";
@@ -63,7 +63,7 @@ const Profile: FC = () => {
         subtitle={t("profile.error-subtitle")}
         error={{
           name: t("profile.error"),
-          message: t("profile.error-message"),
+          message: t("profile.error-message")
         }}
       />
     );
@@ -71,7 +71,7 @@ const Profile: FC = () => {
 
   const extensionProps = {
     me,
-    url,
+    url
   };
 
   return (
@@ -79,10 +79,9 @@ const Profile: FC = () => {
       <Page title={me.displayName}>
         <CustomQueryFlexWrappedColumns>
           <PrimaryContentColumn>
-            <Route path={url} exact>
+            <Route path={`${url}`} exact>
               <ProfileInfo me={me} />
             </Route>
-            <Redirect exact from={`${url}/settings/`} to={`${url}/settings/theme`} />
             <Route path={`${url}/settings/theme`} exact>
               <Theme />
             </Route>
