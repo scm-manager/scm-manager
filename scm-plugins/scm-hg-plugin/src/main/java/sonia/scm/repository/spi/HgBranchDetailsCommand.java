3ef85f6ee121a3d9ad27655750c77b804da7ce37
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2021-12-01 08:34:44 +0100

    Remove changeset style optimization for hg

--- scm-plugins/scm-hg-plugin/src/main/java/sonia/scm/repository/spi/HgBranchDetailsCommand.java
+++ scm-plugins/scm-hg-plugin/src/main/java/sonia/scm/repository/spi/HgBranchDetailsCommand.java
@@ -36,7 +36,6 @@ import java.util.List;
 import static org.javahg.commands.flags.LogCommandFlags.on;
 import static sonia.scm.ContextEntry.ContextBuilder.entity;
 import static sonia.scm.NotFoundException.notFound;
-import static sonia.scm.repository.spi.javahg.AbstractChangesetCommand.CHANGESET_LAZY_STYLE_PATH;
 
 public class HgBranchDetailsCommand implements BranchDetailsCommand {
 
@@ -76,7 +75,6 @@ public class HgBranchDetailsCommand implements BranchDetailsCommand {
         reference
       )
     );
-    logCommand.cmdAppend("--style", CHANGESET_LAZY_STYLE_PATH);
     return logCommand.execute();
   }
 
--- scm-plugins/scm-hg-plugin/src/main/java/sonia/scm/repository/spi/javahg/AbstractChangesetCommand.java
+++ scm-plugins/scm-hg-plugin/src/main/java/sonia/scm/repository/spi/javahg/AbstractChangesetCommand.java
@@ -64,7 +64,7 @@ public abstract class AbstractChangesetCommand extends AbstractCommand
   private static final byte[] CHANGESET_PATTERN = Utils.randomBytes();
 
   /** Field description */
-  public static final String CHANGESET_LAZY_STYLE_PATH =
+  protected static final String CHANGESET_LAZY_STYLE_PATH =
     Utils.resourceAsFile("/sonia/scm/styles/changesets-lazy.style",
       ImmutableMap.of("pattern", CHANGESET_PATTERN)).getPath();
 
