8425ed13b7979d464789dd26b5d7f63b54e0b1a9
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2021-12-09 16:22:33 +0100

    Check whether details are supported

--- scm-webapp/src/main/java/sonia/scm/api/v2/resources/BranchToBranchDtoMapper.java
+++ scm-webapp/src/main/java/sonia/scm/api/v2/resources/BranchToBranchDtoMapper.java
@@ -35,6 +35,9 @@ import sonia.scm.repository.NamespaceAndName;
 import sonia.scm.repository.Person;
 import sonia.scm.repository.Repository;
 import sonia.scm.repository.RepositoryPermissions;
+import sonia.scm.repository.api.Command;
+import sonia.scm.repository.api.RepositoryService;
+import sonia.scm.repository.api.RepositoryServiceFactory;
 import sonia.scm.web.EdisonHalAppender;
 
 import javax.inject.Inject;
@@ -50,6 +53,8 @@ public abstract class BranchToBranchDtoMapper extends HalAppenderMapper implemen
 
   @Inject
   private ResourceLinks resourceLinks;
+  @Inject
+  private RepositoryServiceFactory serviceFactory;
 
   @Mapping(target = "attributes", ignore = true) // We do not map HAL attributes
   public abstract BranchDto map(Branch branch, @Context Repository repository);
@@ -63,11 +68,16 @@ public abstract class BranchToBranchDtoMapper extends HalAppenderMapper implemen
       .self(resourceLinks.branch().self(repository.getNamespace(), repository.getName(), branch.getName()))
       .single(linkBuilder("history", resourceLinks.branch().history(repository.getNamespace(), repository.getName(), branch.getName())).build())
       .single(linkBuilder("changeset", resourceLinks.changeset().changeset(namespaceAndName.getNamespace(), namespaceAndName.getName(), branch.getRevision())).build())
-      .single(linkBuilder("source", resourceLinks.source().self(namespaceAndName.getNamespace(), namespaceAndName.getName(), branch.getRevision())).build())
-      .single(linkBuilder("details", resourceLinks.branchDetails().self(namespaceAndName.getNamespace(), namespaceAndName.getName(), branch.getName())).build());
+      .single(linkBuilder("source", resourceLinks.source().self(namespaceAndName.getNamespace(), namespaceAndName.getName(), branch.getRevision())).build());
 
     if (!branch.isDefaultBranch() && RepositoryPermissions.push(repository).isPermitted()) {
       linksBuilder.single(linkBuilder("delete", resourceLinks.branch().delete(repository.getNamespace(), repository.getName(), branch.getName())).build());
+    }
+
+    try (RepositoryService service = serviceFactory.create(repository)) {
+      if (service.isSupported(Command.BRANCH_DETAILS)) {
+        linksBuilder.single(linkBuilder("details", resourceLinks.branchDetails().self(namespaceAndName.getNamespace(), namespaceAndName.getName(), branch.getName())).build());
+      }
     }
 
     Embedded.Builder embeddedBuilder = Embedded.embeddedBuilder();
