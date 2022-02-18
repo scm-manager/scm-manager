a50e38077cf4c2e405c06a0c4e63f7f55a46c169
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2022-02-18 08:18:42 +0100

    Fix npe for searchable types which no longer exist because the plugin that provided the type was deleted

--- scm-webapp/src/main/java/sonia/scm/search/SearchableTypeResolver.java
+++ scm-webapp/src/main/java/sonia/scm/search/SearchableTypeResolver.java
@@ -86,6 +86,9 @@ class SearchableTypeResolver {
   }
 
   public LuceneSearchableType resolve(Class<?> type) {
+    if (type == null) {
+      throw notFound(entity("type", "null"));
+    }
     LuceneSearchableType searchableType = classToSearchableType.get(type);
     if (searchableType == null) {
       throw notFound(entity("type", type.getName()));
