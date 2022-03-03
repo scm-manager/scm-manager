4d7980124f1542d449da06da2436cee69cea150a
Matthias Thieroff <matthias.thieroff@cloudogu.com>
2022-03-03 14:25:20 +0100

    Sort stories in tree view (#1970)

--- scm-ui/ui-components/.storybook/preview.js
+++ scm-ui/ui-components/.storybook/preview.js
@@ -90,4 +90,7 @@ export const parameters = {
       { name: "highcontrast", color: "#000" },
     ],
   },
+  options: {
+    storySort: (a, b) => (a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true })),
+  },
 };
