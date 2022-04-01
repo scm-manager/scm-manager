8eb65abdaa21b72721f44207d8f3bd7bc099455a
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2022-04-01 10:26:59 +0200

    Cleanup

--- scm-webapp/src/main/java/sonia/scm/validation/GuiceConstraintValidatorFactory.java
+++ scm-webapp/src/main/java/sonia/scm/validation/GuiceConstraintValidatorFactory.java
@@ -46,6 +46,6 @@ public class GuiceConstraintValidatorFactory implements ConstraintValidatorFacto
 
   @Override
   public void releaseInstance(ConstraintValidator<?, ?> instance) {
-    // nothing todo here
+    // do nothing
   }
 }
