bf3696c4fe71f0d3938a115250b99d7ef47f92b0
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2022-04-05 16:43:25 +0200

    Add command for group creation

---
+++ scm-webapp/src/main/java/sonia/scm/group/cli/GroupCreateCommand.java
@@ -0,0 +1,74 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020-present Cloudogu GmbH and Contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package sonia.scm.group.cli;
+
+import picocli.CommandLine;
+import sonia.scm.cli.CommandValidator;
+import sonia.scm.cli.ParentCommand;
+import sonia.scm.group.Group;
+import sonia.scm.group.GroupManager;
+import sonia.scm.repository.cli.GroupCommand;
+
+import javax.inject.Inject;
+
+@ParentCommand(GroupCommand.class)
+@CommandLine.Command(name = "create")
+public class GroupCreateCommand implements Runnable {
+
+  @CommandLine.Mixin
+  private final GroupTemplateRenderer templateRenderer;
+  @CommandLine.Mixin
+  private final CommandValidator validator;
+  private final GroupManager manager;
+
+  @CommandLine.Parameters(descriptionKey = "scm.group.create.name")
+  private String name;
+
+  @CommandLine.Option(names = {"--description", "-d"}, descriptionKey = "scm.group.create.desc")
+  private String description;
+
+  @CommandLine.Option(names = {"--member", "-m"}, descriptionKey = "scm.group.create.member")
+  private String[] members;
+
+  @CommandLine.Option(names = {"--external", "-e"}, descriptionKey = "scm.group.create.external")
+  private boolean external;
+
+  @Inject
+  public GroupCreateCommand(GroupTemplateRenderer templateRenderer, CommandValidator validator, GroupManager manager) {
+    this.templateRenderer = templateRenderer;
+    this.validator = validator;
+    this.manager = manager;
+  }
+
+  @Override
+  public void run() {
+    validator.validate();
+    Group newGroup = new Group("xml", name, members);
+    newGroup.setDescription(description);
+    newGroup.setExternal(external);
+    Group createdGroup = manager.create(newGroup);
+    templateRenderer.render(createdGroup);
+  }
+}
