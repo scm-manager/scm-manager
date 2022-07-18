46eb6edff836fb3729647da4844822232716d763
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2022-07-18 11:27:06 +0200

    Add clear permissions command

---
+++ scm-webapp/src/main/java/sonia/scm/repository/cli/RepositoryClearPermissionsCommand.java
@@ -0,0 +1,82 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020-present Cloudogu GmbH and Contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package sonia.scm.repository.cli;
+
+import com.google.common.annotations.VisibleForTesting;
+import picocli.CommandLine;
+import sonia.scm.cli.ParentCommand;
+import sonia.scm.repository.NamespaceAndName;
+import sonia.scm.repository.Repository;
+import sonia.scm.repository.RepositoryManager;
+
+import javax.inject.Inject;
+
+@CommandLine.Command(name = "clear-permissions")
+@ParentCommand(value = RepositoryCommand.class)
+class RepositoryClearPermissionsCommand implements Runnable {
+
+  private final RepositoryManager repositoryManager;
+
+  @CommandLine.Parameters(paramLabel = "namespace/name", index = "0", descriptionKey = "scm.repo.clear-permissions.repository")
+   private String repository;
+  @CommandLine.Parameters(paramLabel = "name", index = "1", descriptionKey = "scm.repo.clear-permissions.name")
+  private String name;
+
+  @CommandLine.Option(names = {"--group", "-g"}, descriptionKey = "scm.repo.clear-permissions.forGroup")
+  private boolean forGroup;
+
+  @Inject
+  public RepositoryClearPermissionsCommand(RepositoryManager repositoryManager) {
+    this.repositoryManager = repositoryManager;
+  }
+
+  @Override
+  public void run() {
+    NamespaceAndName namespaceAndName = NamespaceAndName.fromString(repository);
+    Repository repo = repositoryManager.get(namespaceAndName);
+
+    if (!forGroup) {
+      repo.findUserPermission(name).ifPresent(repo::removePermission);
+    } else {
+      repo.findGroupPermission(name).ifPresent(repo::removePermission);
+    }
+
+    repositoryManager.modify(repo);
+  }
+
+  @VisibleForTesting
+  void setRepository(String repository) {
+    this.repository = repository;
+  }
+
+  @VisibleForTesting
+  void setName(String name) {
+    this.name = name;
+  }
+
+  public void setForGroup(boolean forGroup) {
+    this.forGroup = forGroup;
+  }
+}
---
+++ scm-webapp/src/test/java/sonia/scm/repository/cli/RepositoryClearPermissionsCommandTest.java
@@ -0,0 +1,101 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020-present Cloudogu GmbH and Contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package sonia.scm.repository.cli;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import sonia.scm.repository.NamespaceAndName;
+import sonia.scm.repository.Repository;
+import sonia.scm.repository.RepositoryManager;
+import sonia.scm.repository.RepositoryPermission;
+import sonia.scm.repository.RepositoryTestData;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class RepositoryClearPermissionsCommandTest {
+
+  @Mock
+  private RepositoryManager repositoryManager;
+
+  @InjectMocks
+  private RepositoryClearPermissionsCommand command;
+
+  private final Repository repository = RepositoryTestData.createHeartOfGold();
+
+  @BeforeEach
+  void mockRepository() {
+    when(repositoryManager.get(new NamespaceAndName("hitchhiker", "HeartOfGold")))
+      .thenReturn(repository);
+  }
+
+  @Test
+  void shouldClearPermissionsForUser() {
+    repository.setPermissions(
+      List.of(
+        new RepositoryPermission("trillian", List.of("read"), false)
+      )
+    );
+
+    command.setRepository("hitchhiker/HeartOfGold");
+    command.setName("trillian");
+
+    command.run();
+
+    verify(repositoryManager).modify(argThat(argument -> {
+      assertThat(argument.getPermissions()).isEmpty();
+      return true;
+    }));
+  }
+
+  @Test
+  void shouldClearPermissionsForGroup() {
+    repository.setPermissions(
+      List.of(
+        new RepositoryPermission("hog", "READ", true)
+      )
+    );
+
+    command.setRepository("hitchhiker/HeartOfGold");
+    command.setName("hog");
+    command.setForGroup(true);
+
+    command.run();
+
+    verify(repositoryManager).modify(argThat(argument -> {
+      assertThat(argument.getPermissions()).isEmpty();
+      return true;
+    }));
+  }
+}
