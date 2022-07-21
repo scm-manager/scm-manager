dd83c90ff1861d82b2f2e4fe8893469916a70f66
Konstantin Schaper <konstantin.schaper@cloudogu.com>
2022-07-21 13:59:07 +0200

    use pre-release version of new eslint config with tailwind support

--- scm-ui/ui-buttons/src/variants/button.tsx
+++ scm-ui/ui-buttons/src/variants/button.tsx
@@ -37,18 +37,24 @@ export const ButtonVariantList = Object.values(ButtonVariants);
 
 export type ButtonVariant = typeof ButtonVariants[keyof typeof ButtonVariants];
 
-const BASE_BUTTON_CLASSES =
-  "font-semibold border rounded py-2 px-12 inline-block text-center disabled:opacity-50 disabled:cursor-not-allowed";
-const DEFAULT_BUTTON_CLASSES =
-  "border-gray-200 hover:border-gray-400 active:shadow-inner disabled:active:shadow-none disabled:hover:border-gray-200";
-const PRIMARY_BUTTON_CLASSES =
-  "bg-primary hover:bg-primary-hover active:bg-primary-active disabled:hover:bg-primary disabled:active:bg-primary text-primary-contrast border-transparent";
-const SECONDARY_BUTTON_CLASSES =
-  "text-primary hover:text-primary-hover active:text-primary-active disabled:hover:text-primary disabled:active:text-primary border-primary hover:border-primary-hover active:border-primary-active disabled:hover:border-primary disabled:active:border-primary";
-const TERTIARY_BUTTON_CLASSES =
-  "text-primary disabled:text-text hover:bg-primary-hover-contrast active:bg-primary-active-contrast disabled:hover:bg-transparent disabled:active:bg-transparent border-transparent";
-const SIGNAL_BUTTON_CLASSES =
-  "bg-signal hover:bg-signal-hover active:bg-signal-active disabled:hover:bg-signal disabled:active:bg-signal text-signal-contrast border-transparent";
+const BASE_BUTTON_CLASSES = classNames(
+  "inline-block rounded border py-2 px-12 text-center font-semibold disabled:cursor-not-allowed disabled:opacity-50"
+);
+const DEFAULT_BUTTON_CLASSES = classNames(
+  "border-gray-200 hover:border-gray-400 active:shadow-inner disabled:hover:border-gray-200 disabled:active:shadow-none"
+);
+const PRIMARY_BUTTON_CLASSES = classNames(
+  "border-transparent bg-primary text-primary-contrast hover:bg-primary-hover active:bg-primary-active disabled:hover:bg-primary disabled:active:bg-primary"
+);
+const SECONDARY_BUTTON_CLASSES = classNames(
+  "border-primary text-primary hover:border-primary-hover hover:text-primary-hover active:border-primary-active active:text-primary-active disabled:hover:border-primary disabled:hover:text-primary disabled:active:border-primary disabled:active:text-primary"
+);
+const TERTIARY_BUTTON_CLASSES = classNames(
+  "border-transparent text-primary hover:bg-primary-hover-contrast active:bg-primary-active-contrast disabled:hover:bg-transparent disabled:active:bg-transparent"
+);
+const SIGNAL_BUTTON_CLASSES = classNames(
+  "border-transparent bg-signal text-signal-contrast hover:bg-signal-hover active:bg-signal-active disabled:hover:bg-signal disabled:active:bg-signal"
+);
 
 export const createButtonClasses = (variant?: ButtonVariant) =>
   classNames(BASE_BUTTON_CLASSES, {
