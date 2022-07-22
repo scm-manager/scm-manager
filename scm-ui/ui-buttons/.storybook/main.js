61536fd457e73a84bc2811fefeaac2bdd9e78fbe
Konstantin Schaper <konstantin.schaper@cloudogu.com>
2022-07-22 16:44:38 +0200

    integrate a11y addon

--- scm-ui/ui-buttons/.storybook/main.js
+++ scm-ui/ui-buttons/.storybook/main.js
@@ -52,6 +52,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y"
   ],
   framework: "@storybook/react",
   webpackFinal: async (config) => {
