8bc333f33004e6ade7d732b075751d9adf6f1b25
Konstantin Schaper <konstantin.schaper@cloudogu.com>
2022-07-26 14:13:52 +0200

    implement dark & high contrast colors

--- scm-ui/ui-buttons/src/button.tsx
+++ scm-ui/ui-buttons/src/button.tsx
@@ -38,7 +38,7 @@ export const ButtonVariantList = Object.values(ButtonVariants);
 type ButtonVariant = typeof ButtonVariants[keyof typeof ButtonVariants];
 
 const BASE_BUTTON_CLASSES = classNames(
-  "inline-block rounded border py-2 px-6 text-center font-semibold focus:outline-none focus:ring focus:ring-purple-500 focus:ring-offset-2 disabled:cursor-not-allowed"
+  "inline-block rounded border py-2 px-6 text-center font-semibold focus:outline focus:outline-purple-500 focus:outline-offset-2 focus:z-10 disabled:cursor-not-allowed"
 );
 const DEFAULT_BUTTON_CLASSES = classNames(
   "border-gray-200 hover:border-gray-400 active:shadow-inner disabled:hover:border-gray-200 disabled:active:shadow-none"
@@ -50,7 +50,7 @@ const SECONDARY_BUTTON_CLASSES = classNames(
   "border-primary text-primary hover:border-primary-hover hover:text-primary-hover active:border-primary-active active:text-primary-active disabled:border-primary-disabled disabled:text-primary-disabled"
 );
 const TERTIARY_BUTTON_CLASSES = classNames(
-  "border-transparent text-primary hover:bg-primary-hover-contrast hover:text-primary-hover active:bg-primary-active-contrast active:text-primary-active disabled:text-primary-disabled"
+  "border-transparent text-primary hover:text-primary-hover active:text-primary-active disabled:text-primary-disabled"
 );
 const SIGNAL_BUTTON_CLASSES = classNames(
   "border-transparent bg-signal text-signal-contrast hover:bg-signal-hover hover:text-signal-hover-contrast active:bg-signal-active active:text-signal-active-contrast disabled:bg-signal-disabled disabled:text-signal-disabled-contrast"
