5842f508e87f4b24d865b40e3ce49290529335d7
Konstantin Schaper <konstantin.schaper@cloudogu.com>
2022-11-07 16:24:19 +0100

    implement group list navigation

--- scm-ui/ui-webapp/src/groups/components/table/GroupRow.tsx
+++ scm-ui/ui-webapp/src/groups/components/table/GroupRow.tsx
@@ -21,40 +21,41 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-import React from "react";
-import { WithTranslation, withTranslation } from "react-i18next";
+import React, { FC } from "react";
+import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 import { Group } from "@scm-manager/ui-types";
 import { Icon } from "@scm-manager/ui-components";
 import classNames from "classnames";
+import { useKeyboardIteratorTarget } from "@scm-manager/ui-shortcuts";
 
-type Props = WithTranslation & {
+type Props = {
   group: Group;
 };
 
-class GroupRow extends React.Component<Props> {
-  renderLink(to: string, label: string) {
-    return <Link to={to}>{label}</Link>;
-  }
+const GroupRow: FC<Props> = ({ group }) => {
+  const ref = useKeyboardIteratorTarget();
+  const [t] = useTranslation("groups");
+  const to = `/group/${group.name}`;
+  const iconType = group.external ? (
+    <Icon title={t("group.external")} name="globe-americas" />
+  ) : (
+    <Icon title={t("group.internal")} name="home" />
+  );
 
-  render() {
-    const { group, t } = this.props;
-    const to = `/group/${group.name}`;
-    const iconType = group.external ? (
-      <Icon title={t("group.external")} name="globe-americas" />
-    ) : (
-      <Icon title={t("group.internal")} name="home" />
-    );
+  return (
+    <tr>
+      <td className="is-word-break">
+        {iconType}{" "}
+        {
+          <Link ref={ref} to={to}>
+            {group.name}
+          </Link>
+        }
+      </td>
+      <td className={classNames("is-hidden-mobile", "is-word-break")}>{group.description}</td>
+    </tr>
+  );
+};
 
-    return (
-      <tr>
-        <td className="is-word-break">
-          {iconType} {this.renderLink(to, group.name)}
-        </td>
-        <td className={classNames("is-hidden-mobile", "is-word-break")}>{group.description}</td>
-      </tr>
-    );
-  }
-}
-
-export default withTranslation("groups")(GroupRow);
+export default GroupRow;
