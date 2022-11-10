scm-manager/scm-manager

commit af9aaec0955c720cc9b24ccd88c09f018ec2d11f

Add keyboard shortcut for global search (#2118)
Enable users to jump to the global search bar by pressing the "/" key from anywhere. Open modals block this shortcut. This PR also introduces a generic system for declaring global shortcuts by utilizing the third-party library mousetrap.

scm-ui/ui-components/src/modals/activeModalCountContext.ts
@@ -22,10 +22,8 @@
  * SOFTWARE.
  */
 
-// @create-index
+import React from "react";
 
-export { default as ConfirmAlert, confirmAlert } from "./ConfirmAlert";
-export { default as Modal } from "./Modal";
-export { default as FullscreenModal } from "./FullscreenModal";
-export { default as ActiveModalCountContextProvider } from "./ActiveModalCountContextProvider";
-export { default as useActiveModals } from "./useActiveModals";
+export type ModalStateContextType = { value: number; increment: () => void; decrement: () => void };
+
+export default React.createContext<ModalStateContextType>({} as ModalStateContextType);

scm-ui/ui-api/src/help/search/modal.de.ts
@@ -30,4 +30,8 @@ Um mehr über die fortgeschrittene Suche zu erfahren, lesen sie unsere [Experten
 - Die relevantesten Repositories werden in den Quick Results angezeigt.
 - Über die Eingabe-Taste oder den Button "Alle Ergebnisse anzeigen" bekommen Sie Ergebnisse aller durchsuchten Entitäten wie Nutzern oder Gruppen.
 - Eine Wildcard für eine beliebige Anzahl an beliebigen Zeichen wird Ihrer Suche standardmäßig angehängt.
-- Geben Sie keine Wildcards vor dem Suchbegriff ein!`;
+- Geben Sie keine Wildcards vor dem Suchbegriff ein!
+
+### Tastenkombinationen
+
+Sie können von überall aus "/" drücken, um den Tastaturfokus auf die globale Suchleiste bewegen.`;

scm-ui/ui-api/src/help/search/modal.en.ts
@@ -30,4 +30,9 @@ To learn about advanced search read our [Expert Search Site](/help/search-syntax
 - The most relevant repositories are shown in the quick results.
 - Press "enter" or click the "Show all results" button to find more results for all entities like users or groups.
 - A multi-character wildcard (*) is added to your search by default.
-- Do not enter Wildcards in front of the search!`;
+- Do not enter Wildcards in front of the search!
+
+### Shortcuts
+
+You can press "/" from anywhere to move keyboard focus to the global search bar.
+`;

scm-ui/ui-components/src/modals/ActiveModalCountContextProvider.tsx
@@ -22,10 +22,28 @@
  * SOFTWARE.
  */
 
-// @create-index
+import ActiveModalCountContext from "./activeModalCountContext";
+import React, { FC, useCallback, useState } from "react";
 
-export { default as ConfirmAlert, confirmAlert } from "./ConfirmAlert";
-export { default as Modal } from "./Modal";
-export { default as FullscreenModal } from "./FullscreenModal";
-export { default as ActiveModalCountContextProvider } from "./ActiveModalCountContextProvider";
-export { default as useActiveModals } from "./useActiveModals";
+/**
+ * A simple counter that allows developers to keep track of how many modals are currently open.
+ *
+ * A default provider instance wraps the SCM-Manager so there is no need for plugins to use this component.
+ *
+ * Required by {@link useActiveModals}.
+ */
+const ActiveModalCountContextProvider: FC = ({ children }) => {
+  const [activeModalCount, setActiveModalCount] = useState(0);
+  const incrementModalCount = useCallback(() => setActiveModalCount((prev) => prev + 1), []);
+  const decrementModalCount = useCallback(() => setActiveModalCount((prev) => prev - 1), []);
+
+  return (
+    <ActiveModalCountContext.Provider
+      value={{ value: activeModalCount, increment: incrementModalCount, decrement: decrementModalCount }}
+    >
+      {children}
+    </ActiveModalCountContext.Provider>
+  );
+};
+
+export default ActiveModalCountContextProvider;

scm-ui/ui-components/src/modals/ConfirmAlert.stories.tsx
@@ -26,6 +26,7 @@ import { storiesOf } from "@storybook/react";
 import { MemoryRouter } from "react-router-dom";
 import * as React from "react";
 import ConfirmAlert, { confirmAlert } from "./ConfirmAlert";
+import ActiveModalCountContext from "./activeModalCountContext";
 
 const body =
   "Mind-paralyzing change needed improbability vortex machine sorts sought same theory upending job just allows\n " +
@@ -37,27 +38,36 @@ const buttons = [
   {
     className: "is-outlined",
     label: "Cancel",
-    onClick: () => null
+    onClick: () => null,
   },
   {
-    label: "Submit"
-  }
+    label: "Submit",
+  },
 ];
 
 const buttonsWithAutofocus = [
   {
     label: "Cancel",
-    onClick: () => null
+    onClick: () => null,
   },
   {
     className: "is-info",
     label: "I should be focused",
-    autofocus: true
-  }
+    autofocus: true,
+  },
 ];
 
+const doNothing = () => {
+  // Do nothing
+};
+
 storiesOf("Modal/ConfirmAlert", module)
-  .addDecorator(story => <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>)
+  .addDecorator((story) => <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>)
+  .addDecorator((story) => (
+    <ActiveModalCountContext.Provider value={{ value: 0, increment: doNothing, decrement: doNothing }}>
+      {story()}
+    </ActiveModalCountContext.Provider>
+  ))
   .add("Default", () => <ConfirmAlert message={body} title={"Are you sure about that?"} buttons={buttons} />)
   .add("WithButton", () => {
     const buttonClick = () => {

scm-ui/ui-components/src/modals/Modal.stories.tsx
@@ -34,6 +34,7 @@ import { Button, ButtonGroup } from "../buttons";
 import Notification from "../Notification";
 import { Autocomplete } from "../index";
 import { SelectValue } from "@scm-manager/ui-types";
+import ActiveModalCountContext from "./activeModalCountContext";
 
 const TopAndBottomMargin = styled.div`
   margin: 0.75rem 0; // only for aesthetic reasons
@@ -75,20 +76,25 @@ const withFormElementsFooter = (
 );
 
 const loadSuggestions: (p: string) => Promise<SelectValue[]> = () =>
-  new Promise(resolve => {
+  new Promise((resolve) => {
     setTimeout(() => {
       resolve([
         { value: { id: "trillian", displayName: "Tricia McMillan" }, label: "Tricia McMillan" },
         { value: { id: "zaphod", displayName: "Zaphod Beeblebrox" }, label: "Zaphod Beeblebrox" },
         { value: { id: "ford", displayName: "Ford Prefect" }, label: "Ford Prefect" },
         { value: { id: "dent", displayName: "Arthur Dent" }, label: "Arthur Dent" },
-        { value: { id: "marvin", displayName: "Marvin" }, label: "Marvin the Paranoid Android " }
+        { value: { id: "marvin", displayName: "Marvin" }, label: "Marvin the Paranoid Android " },
       ]);
     });
   });
 
 storiesOf("Modal/Modal", module)
-  .addDecorator(story => <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>)
+  .addDecorator((story) => <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>)
+  .addDecorator((story) => (
+    <ActiveModalCountContext.Provider value={{ value: 0, increment: doNothing, decrement: doNothing }}>
+      {story()}
+    </ActiveModalCountContext.Provider>
+  ))
   .add("Default", () => (
     <NonCloseableModal>
       <p>{text}</p>

scm-ui/ui-components/src/modals/Modal.tsx
@@ -26,6 +26,7 @@ import classNames from "classnames";
 import styled from "styled-components";
 import { Dialog } from "@headlessui/react";
 import { devices } from "../devices";
+import useRegisterModal from "./useRegisterModal";
 
 type ModalSize = "S" | "M" | "L";
 
@@ -67,6 +68,7 @@ export const Modal: FC<Props> = ({
   initialFocusRef,
   overflowVisible
 }) => {
+  useRegisterModal(active);
   const closeButtonRef = useRef<HTMLButtonElement | null>(null);
   let showFooter = null;
 

scm-ui/ui-components/src/modals/useActiveModals.ts
@@ -22,10 +22,15 @@
  * SOFTWARE.
  */
 
-// @create-index
+import { useContext } from "react";
+import ActiveModalCountContext from "./activeModalCountContext";
 
-export { default as ConfirmAlert, confirmAlert } from "./ConfirmAlert";
-export { default as Modal } from "./Modal";
-export { default as FullscreenModal } from "./FullscreenModal";
-export { default as ActiveModalCountContextProvider } from "./ActiveModalCountContextProvider";
-export { default as useActiveModals } from "./useActiveModals";
+/**
+ * @returns Whether any modals are currently open
+ */
+const useActiveModals = () => {
+  const { value } = useContext(ActiveModalCountContext);
+  return value > 0;
+};
+
+export default useActiveModals;

commit 86f95ebb35481eb5f000919bf2f63a80ea4f2e11

Fix regression introduced in af9aaec0955c720cc9b24ccd88c09f018ec2d11f
ConfirmDialogs created through the deprecated confirmDialog function are mounted outside the react root and are therefore missing the required context. A default context with a warning has been added to prevent the null pointer exception.

scm-ui/ui-components/src/modals/activeModalCountContext.ts
@@ -26,4 +26,15 @@ import React from "react";
 
 export type ModalStateContextType = { value: number; increment: () => void; decrement: () => void };
 
-export default React.createContext<ModalStateContextType>({} as ModalStateContextType);
+export default React.createContext<ModalStateContextType>({
+  value: 0,
+  increment: () => {
+    // eslint-disable-next-line no-console
+    console.warn(
+      "Modals should be declared inside a ModalStateContext. Did you use the deprecated 'confirmAlert' function over the 'ConfirmAlert' component ?"
+    );
+  },
+  decrement: () => {
+    /* Do nothing */
+  },
+});