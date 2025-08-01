3d75f5f791b0a62a050419ead4fca9674f7d5a6c
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2025-08-01 13:48:42 +0200

    Fix release script

--- release.sh
+++ release.sh
@@ -23,5 +23,5 @@ git checkout -b release/${NEW_VERSION}
 git add CHANGELOG.md
 git commit -m "Adjust changelog for release ${NEW_VERSION}"
 git push origin release/${NEW_VERSION}
-xdg-open https://ecosystem.cloudogu.com/jenkins/view/SCMM/job/scm-manager/job/scm-manager/job/release%252F${VERSION}/
+xdg-open https://ecosystem.cloudogu.com/jenkins/view/SCMM/job/scm-manager/job/scm-manager/job/release%252F${NEW_VERSION}/
 
